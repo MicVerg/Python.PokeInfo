0ebc832e1edc43a739e101b365eb919162f995af
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-04-13 20:09:44 +0000

    Thu, Apr 13, 2023, 10:09 PM +02:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -4,6 +4,11 @@
 int main(void)
 {
     //ask user height integer
-
+    int height;
+    do
+    {
+        height = get_int("What height do you want the stairs to be? ");
+    }
+    while (height > 0 && height < 9);
     //based on height integer, loop that many times, adding #
 }
