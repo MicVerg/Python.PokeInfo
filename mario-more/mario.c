8090a24c42fac32abc2daf66026a4f1877d762d5
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-04-17 12:05:48 +0000

    Mon, Apr 17, 2023, 2:05 PM +02:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    //ask user height integer
+    int height;
+    do
+    {
+        height = get_int("What height do you want the stairs to be? ");
+    }
+    while (height < 1 || height > 8);
 }
