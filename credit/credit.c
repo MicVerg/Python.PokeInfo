88b1d495929d86c144c704c143bd0c5d137f29bd
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-04-17 18:47:04 +0000

    Mon, Apr 17, 2023, 8:47 PM +02:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,7 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    //american express 15 digits, starts with 34 or 37
+
+    //mastercard 16 digits, starts with 51 52 53 54 55
+
+    //visa 13 or 16 digits, starts with 4
+
+    //checksum 
 }
