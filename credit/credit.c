e8da229091e0c78a106b2dc044c52cbbea1b9d3a
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-04-17 18:52:19 +0000

    Mon, Apr 17, 2023, 8:52 PM +02:00

--- credit/credit.c
+++ credit/credit.c
@@ -3,13 +3,17 @@
 
 int main(void)
 {
+    //prompt
+
+    //checksum
+
     //american express 15 digits, starts with 34 or 37
 
     //mastercard 16 digits, starts with 51 52 53 54 55
 
     //visa 13 or 16 digits, starts with 4
 
-    //checksum
-
     //modulo 10
+
+    //print result
 }
