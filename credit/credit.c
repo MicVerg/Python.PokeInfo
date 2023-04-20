292ba96b5a5da6bce47162e9eab5c9fa208a8500
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-04-20 08:36:00 +0000

    Thu, Apr 20, 2023, 10:36 AM +02:00

--- credit/credit.c
+++ credit/credit.c
@@ -20,9 +20,9 @@ int main(void)
     printf("amount of digits: %i\n", amount);
 
     //loop
-    for (int secondtolast = 0; secondtolast < amount; secondtolast++)
+    for (int sum = 0; sum < amount; sum++)
     {
-        
+
     }
 
     //checksum
