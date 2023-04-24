6505ea048f117a43fad4d01490c65510e90728a8
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-04-24 15:26:30 +0000

    Mon, Apr 24, 2023, 5:26 PM +02:00

--- readability/readability.c
+++ readability/readability.c
@@ -8,6 +8,7 @@ int main(void)
 //use formula to calculate grade
 //index = 0.0588 * L - 0.296 * S - 15.8
 //L average number of letters per 100 words - S average number of sentences per 100 words
+//use floats to calc these
 
 }
 
