06cdabaa3627334496609a82886d0bed644d3e8c
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-04-25 16:10:30 +0000

    Tue, Apr 25, 2023, 6:10 PM +02:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -5,12 +5,14 @@ int main(int argc, string argv[])
 {
     //get input from cl
     //should be one argument in cl
+    if (argc < )
+    printf("Usage: ./caesar key\n");
 
     //check if input is actually numeral, not alphabetical, not negative, nor empty nor multiple arguments
     //remind user with usage: ./caesar key
 
     //prompt user for plaintext
-    
+
     //move letters by input key, ONLY alphabetical chars tho
     //so change every character to character + key
 
