41aa42f085a04275335d96284f0ddac3bdb99a89
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-04-25 13:53:14 +0000

    Tue, Apr 25, 2023, 3:53 PM +02:00

--- readability/readability.c
+++ readability/readability.c
@@ -27,7 +27,7 @@ int main(void)
     //use floats to calc these
     float L = (lettertotal / wordtotal) * 100;
     float S = (sentencetotal / wordtotal) * 100;
-    double grade = round(0.0588 * L - 0.296 * S - 15.8);
+    double grade = (0.0588 * L - 0.296 * S - 15.8);
     printf("L: %f\n", L);
     printf("S: %f\n", S);
     printf("grade: %f\n", grade);
