dbdc055352a7e9dedaf66b16e5e8f00a89735561
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-04-25 16:07:31 +0000

    Tue, Apr 25, 2023, 6:07 PM +02:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -9,6 +9,8 @@ int main(int argc, string argv[])
     //check if input is actually numeral, not alphabetical, not negative, nor empty nor multiple arguments
     //remind user with usage: ./caesar key
 
+    //prompt user for plaintext
+    
     //move letters by input key, ONLY alphabetical chars tho
     //so change every character to character + key
 
