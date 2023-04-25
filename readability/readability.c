e1199e2787c28c01a38e819f0ab1ff17fc103723
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-04-25 13:15:21 +0000

    Tue, Apr 25, 2023, 3:15 PM +02:00

--- readability/readability.c
+++ readability/readability.c
@@ -22,7 +22,6 @@ int main(void)
     //index = 0.0588 * L - 0.296 * S - 15.8
     //L average number of letters per 100 words - S average number of sentences per 100 words
     //use floats to calc these
-
 }
 
 //write function to count uppercase and lowercase letters
