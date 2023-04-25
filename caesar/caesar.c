3c6064a7a56a72e76bfe355615f52edfab64c94c
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-04-25 20:56:00 +0000

    Tue, Apr 25, 2023, 10:56 PM +02:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -58,7 +58,7 @@ bool only_digits(string key)
     return false;
 }
 
-char rotate(char c, int n)
+char rotate(char c, int intkey)
 {
     int subtracted = 0;
     int addkey = 0;
@@ -68,13 +68,13 @@ char rotate(char c, int n)
         if (isupper(c))
         {
             subtracted = c - 65;
-            addkey = (subtracted + n) % 26;
+            addkey = (subtracted + intkey) % 26;
             result = char()addkey;
         }
         else if (islower(c))
         {
             subtracted = c - 97;
-            addkey = (subtracted + n) % 26;
+            addkey = (subtracted + intkey) % 26;
             result = char()addkey;
         }
     }
