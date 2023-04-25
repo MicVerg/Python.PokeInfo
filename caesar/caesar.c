db05126e9026f750f1bfae7aeb915a3bce9e2291
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-04-25 14:36:21 +0000

    Tue, Apr 25, 2023, 4:36 PM +02:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,7 +1,12 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
+    //get input from cl
+
+    //move letters by input key, ONLY alphabetical
+    //so change every character to character + key
+    
 
 }
