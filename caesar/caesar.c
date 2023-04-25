9bd0cada180ee0f855a4d7acc3d3184168ad96c7
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-04-25 21:34:58 +0000

    Tue, Apr 25, 2023, 11:34 PM +02:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -37,8 +37,8 @@ int main(int argc, string argv[])
     int length = strlen(plaintext);
     for (int i = 0; i < length; i++)
     {
-        rotate(plaintext[i], intkey);
-        printf("ciphertext: %c\n", result);
+        char deciphered = rotate(plaintext[i], intkey);
+        printf("ciphertext: %c\n", deciphered);
     }
     //key is argv[1]
     //prompt user for plaintext
