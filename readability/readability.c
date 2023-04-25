3c85ea7eb5491d845931830d62c77d016763b157
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-04-25 11:39:47 +0000

    Tue, Apr 25, 2023, 1:39 PM +02:00

--- readability/readability.c
+++ readability/readability.c
@@ -5,6 +5,10 @@ int main(void)
 {
 //prompt for text
 
+printf("letters: %i\n", letters);
+printf("words: %i\n", words);
+printf("sentences: %i\n", sentences);
+
 //use formula to calculate grade
 //index = 0.0588 * L - 0.296 * S - 15.8
 //L average number of letters per 100 words - S average number of sentences per 100 words
@@ -18,13 +22,13 @@ int count_letters(string text)
     int letters = 0;
 }
 
-
 //write function to count words
 //count the spaces (spaces + 1 = total words)
 int count_words(string text)
 {
     int words = 0;
 }
+
 //write function to count sentences (sentences end with . ! ?)
 int count_sentences(string text)
 {
