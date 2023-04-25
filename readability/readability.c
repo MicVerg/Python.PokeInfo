884796ecf780da825159399c6c618a819a34d2f2
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-04-25 11:41:22 +0000

    Tue, Apr 25, 2023, 1:41 PM +02:00

--- readability/readability.c
+++ readability/readability.c
@@ -3,16 +3,17 @@
 
 int main(void)
 {
-//prompt for text
+    //prompt for text
+    string text = get_string("Text: ");
 
-printf("letters: %i\n", letters);
-printf("words: %i\n", words);
-printf("sentences: %i\n", sentences);
+    printf("letters: %i\n", letters);
+    printf("words: %i\n", words);
+    printf("sentences: %i\n", sentences);
 
-//use formula to calculate grade
-//index = 0.0588 * L - 0.296 * S - 15.8
-//L average number of letters per 100 words - S average number of sentences per 100 words
-//use floats to calc these
+    //use formula to calculate grade
+    //index = 0.0588 * L - 0.296 * S - 15.8
+    //L average number of letters per 100 words - S average number of sentences per 100 words
+    //use floats to calc these
 
 }
 
