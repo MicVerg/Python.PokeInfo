59129958f23e4d434950fa942904335abc45881d
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-04-26 19:50:09 +0000

    Wed, Apr 26, 2023, 9:50 PM +02:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -62,12 +62,8 @@ bool only_digits(string key)
         {
             return true;
         }
-        else
-        {
-            return false;
-        }
     }
-
+    return false;
 }
 
 char rotate(char c, int n)
