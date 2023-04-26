603d9709668bfcd7db89af5f0c2e25f8b75ad28a
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-04-26 14:15:54 +0000

    Wed, Apr 26, 2023, 4:15 PM +02:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -30,7 +30,7 @@ int main(int argc, string argv[])
         for (int i = 0; i < length; i++)
         {
             char deciphered = rotate(plaintext[i], intkey);
-            string ciphertext = " ";
+            string ciphertext[];
             ciphertext = strncat(ciphertext, &deciphered, i);
             printf("ciphertext: %s\n", ciphertext);
         }
