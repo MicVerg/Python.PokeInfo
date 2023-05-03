2eee3604762ec558ce95531d9c4f94fbca1705f6
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-03 18:54:26 +0000

    Wed, May 3, 2023, 8:54 PM +02:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -144,7 +144,10 @@ void tabulate(void)
 {
     for (int j = 0; j < voter_count; j++)
     {
-        for (int i = 0; i < )
+        for (int i = 0; i < voter_count; i++)
+        {
+            
+        }
     }
     return;
 }
