9bae893e2c94e94091feb680c7e680d377796ecd
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-03 20:27:54 +0000

    Wed, May 3, 2023, 10:27 PM +02:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -146,9 +146,9 @@ void tabulate(void)
     {
         for (int j = 0; j < candidate_count; j++)
         {
-            if (candidates[j].eliminated == false)
+            if (candidates[preferences[i][j]].eliminated == false)
             {
-                candidates[j].votes++;
+                candidates[preferences[i][j]].votes++;
                 break;
             }
             else if (candidates[j].eliminated == true)
