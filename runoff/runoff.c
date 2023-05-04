473258ba3537ab1379b684a5614ba1288a5f5133
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-04 09:36:04 +0000

    Thu, May 4, 2023, 11:36 AM +02:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -196,7 +196,7 @@ bool is_tie(int min)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].eliminated == false)
+        if (candidates[i].eliminated == false && candidates[i].votes == min)
         {
             int counter = 0;
         }
