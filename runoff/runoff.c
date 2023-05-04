380687965442f2d50a17e5d2a043eae98d9b3c95
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-04 08:19:44 +0000

    Thu, May 4, 2023, 10:19 AM +02:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -172,20 +172,22 @@ bool print_winner(void)
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
+    int j = 0;
     do
     {
-
+        j++;
     }
-    while 
-        int min = candidates[0].votes;
-        for (int i = 0; i < candidate_count; i++)
+    while (candidates[j].eliminated == true);
+
+    int min = candidates[0].votes;
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes < min && candidates[i].eliminated == false)
         {
-            if (candidates[i].votes < min && candidates[i].eliminated == false)
-            {
-                min = candidates[i].votes;
-            }
+            min = candidates[i].votes;
         }
-        return min;
+    }
+    return min;
 }
 
 // Return true if the election is tied between all candidates, false otherwise
