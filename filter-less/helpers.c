801e1716ad0bff5370db782ccda8dc51cb60d9d6
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-06 15:33:31 +0000

    Sat, May 6, 2023, 5:33 PM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -69,6 +69,13 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     //swap horizontally, not vertically
     //2 cases: even and uneven amount of columns
     if (width % 2 == 0)
+    {
+        //do something when even
+    }
+    else if
+    {
+        //do something when uneven
+    }
     return;
 }
 
