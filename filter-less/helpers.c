f84b09adee43fc79eb55b0646fa2702dcd941fad
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-06 15:28:12 +0000

    Sat, May 6, 2023, 5:28 PM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -66,6 +66,8 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    //swap horizontally, not vertically
+    //2 cases: even and uneven amount of columns
     return;
 }
 
