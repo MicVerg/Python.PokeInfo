b5b1a6a23d4f4a86f5fa7770e12ecb27167ab8a0
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-06 14:18:00 +0000

    Sat, May 6, 2023, 4:18 PM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -3,6 +3,15 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; i++)
+        {
+            //calculate each pixel value (average)
+            
+            //set each pixel value to that calculated average
+        }
+    }
     return;
 }
 
