8ddece21e078a5ea2b78d2a4e5bc9ad579c7d4a7
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-06 14:30:13 +0000

    Sat, May 6, 2023, 4:30 PM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -7,7 +7,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     int average = 0;
     for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j < width; i++)
+        for (int j = 0; j < width; j++)
         {
             //calculate average of the 3 pixels
             average = (image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0;
