daa0f61f5cb84f2697bdfe7d77d291d5872d69be
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-07 15:12:50 +0000

    Sun, May 7, 2023, 5:12 PM +02:00

--- reverse/reverse.c
+++ reverse/reverse.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "wav.h"
 
@@ -48,11 +49,12 @@ int main(int argc, char *argv[])
 int check_format(WAVHEADER header)
 {
     // TODO #4
-    for (i = 0; i < 4; i++)
+    if (strcmp(header.format, "WAVE", 4) == 0)
     {
-        
+        return true;
     }
-    return 0;
+return false;
+
 }
 
 int get_block_size(WAVHEADER header)
