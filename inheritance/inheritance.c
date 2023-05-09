45bded3d353cd64111fcf14976de8f39dd7b09f8
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-09 14:27:57 +0000

    Tue, May 9, 2023, 4:27 PM +02:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -42,11 +42,6 @@ person *create_family(int generations)
     // TODO: Allocate memory for new person
     person *p = malloc(sizeof(person));
 
-    if (p != NULL)
-    {
-        return 0;
-    }
-
     // If there are still generations left to create
     if (generations > 1)
     {
