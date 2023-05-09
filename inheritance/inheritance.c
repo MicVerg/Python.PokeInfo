e2357c9f458b625ccff0ff89e7664f7d65229054
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-09 13:47:20 +0000

    Tue, May 9, 2023, 3:47 PM +02:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -52,9 +52,9 @@ person *create_family(int generations)
         // TODO: Set parent pointers for current person
         p -> parents[0] = parent0;
         p -> parents[1] = parent1;
-        
+
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-        person *alleles0 =
+        random_allele(p);
     }
 
     // If there are no generations left to create
