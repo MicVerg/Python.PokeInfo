6102f3d8b5096570ede675ebfee118a3ac64d36a
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-10 11:54:41 +0000

    Wed, May 10, 2023, 1:54 PM +02:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -64,9 +64,9 @@ bool load(const char *dictionary)
     //fscanf(file, "%s", word) ------- FILE = pointer u get from fopen
     //run fscanf on each word of the dictionary
     //until fscanf returns EOF (loop fscanf until EOF)
-    do
+    while (fscanf(*input, "%s", word != EOF))
     {
-        
+
     }
     //Create a new node for each word
     //use malloc -- check if return == NULL -- strcpy word into node
