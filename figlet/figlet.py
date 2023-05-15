87c5aa8bbe58f643fd6e4065af2134a881775b72
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-15 13:43:46 +0000

    Mon, May 15, 2023, 3:43 PM +02:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -7,15 +7,15 @@ figlet = Figlet()
 
 input = get_string("Input: ")
 #print(figlet.getFonts())
-if len(sys.argv) == 0:
+if len(sys.argv) == 1:
     #output the text in random font from figlet.getFonts list
     randomFont = random.choice(figlet.getFonts())
     figlet.setFont(font=randomFont)
     print(figlet.renderText(input))
-elif len(sys.argv) == 2:
+elif len(sys.argv) == 3:
     ##output the text with specific font from command line
-    if sys.argv[1] != "-f" or "--font":
-        print("This is an error message")
+    if sys.argv[1] != "-f" and sys.argv[1] != "--font":
+        print("Usage: python filemane.py -f [fontname]")
         sys.exit(1)
     else:
         figlet.setFont(sys.argv[2])
