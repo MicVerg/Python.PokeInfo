1486c3e1e2190e31387d31c6111d65e8c8a925dc
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-15 12:37:56 +0000

    Mon, May 15, 2023, 2:37 PM +02:00

--- bank/bank.py
+++ bank/bank.py
@@ -4,8 +4,8 @@ from cs50 import get_string
 greeting = input("Greeting: ")
 
 if greeting[0:5].lower() == "hello":
-    print("0$", end="")
+    print("$0", end="")
 elif greeting[0:1].lower() == 'h':
-    print("20$", end="")
+    print("$20", end="")
 else:
-    print("100$", end="")
+    print("$100", end="")
