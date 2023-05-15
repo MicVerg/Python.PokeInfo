87ed5324ff3ad142f73ee4ae63a27aa310ee6a95
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-15 13:58:03 +0000

    Mon, May 15, 2023, 3:58 PM +02:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -6,19 +6,22 @@ import random
 figlet = Figlet()
 
 fontList = figlet.getFonts()
-input = get_string("Input: ")
+
 #print(figlet.getFonts())
 if len(sys.argv) == 1:
+    input = get_string("Input: ")
     #output the text in random font from figlet.getFonts list
     randomFont = random.choice(fontList)
     figlet.setFont(font=randomFont)
     print(figlet.renderText(input))
 elif len(sys.argv) == 3:
+    input = get_string("Input: ")
     ##output the text with specific font from command line
     if sys.argv[1] != "-f" and sys.argv[1] != "--font":
         print("Usage: python figlet.py -f <font>")
         sys.exit(1)
     else:
+        input = get_string("Input: ")
         fontName = sys.argv[2]
         figlet.setFont(font=fontName)
         print(figlet.renderText(input))
