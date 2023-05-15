83fa7e552729b135f682f3a436965144240cc289
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-15 12:23:06 +0000

    Mon, May 15, 2023, 2:23 PM +02:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,8 +1,8 @@
-greeting = input("Greeting: ")
+greeting = input("Greeting: ").lower
 
-if lower.(greeting).startswith("hello") == true:
+if greeting.startswith("hello") == true:
     print("0$")
-elif lower.(greeting).startswith("h", 0):
+elif greeting.startswith("h", 0):
     print("20$")
 else:
     print("100$")
