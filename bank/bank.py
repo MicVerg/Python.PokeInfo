33149ac3e456150fab8865b0b3efd5966f2f5d32
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-15 12:30:24 +0000

    Mon, May 15, 2023, 2:30 PM +02:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,11 +1,11 @@
 from cs50 import get_string
 
 
-greeting = get_string("Greeting: ").lower()
+greeting = get_string("Greeting: ")
 
-if greeting.startswith("hello") == true:
+if greeting[0:5].lower() == "hello":
     print("0$")
-elif greeting.startswith("h", 0):
+elif greeting[0:1].lower() == 'h':
     print("20$")
 else:
     print("100$")
