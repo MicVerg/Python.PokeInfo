8900995d9e4617da834cdbfb0e34dcfbe4139742
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-15 13:12:38 +0000

    Mon, May 15, 2023, 3:12 PM +02:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,6 +1,7 @@
 from pyfiglet import Figlet
 from cs50 import get_string
 from sys import argv
+import random
 
 figlet = Figlet()
 
@@ -8,6 +9,8 @@ input = get_string("Input: ")
 #print(figlet.getFonts())
 if len(argv) == 0:
     #output the text in random font from figlet.getFonts list
+    randomFont = random.choice(figlet.getFonts())
+    
 elif len(argv) == 2:
     ##output the text with specific font from command line
-    
+
