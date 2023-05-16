5455e890a33e1e2add6e7722c66f453609e2dff3
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-16 13:01:48 +0000

    Tue, May 16, 2023, 3:01 PM +02:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -16,7 +16,10 @@ def main():
 
     teams = []
     # TODO: Read teams into memory from file
+    with open(2018m.csv) as file:
+        reader = csv.DictReader(file)
 
+        
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
 
