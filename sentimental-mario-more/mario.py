a1dff6f040a6099f0ad82152d83aae58a6736a35
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-17 09:53:47 +0000

    Wed, May 17, 2023, 11:53 AM +02:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -11,4 +11,4 @@ while True:
 
 
 for i in range(1, height + 1):
-    print(' ' * (height - i) + ('#' * i) + ' ' + ('#' * i) + ' ' * (height - i))
+    print(' ' * (height - i) + ('#' * i) + '  ' + ('#' * i) + ' ' * (height - i))
