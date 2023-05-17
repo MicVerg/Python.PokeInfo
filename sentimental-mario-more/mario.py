71a390e7dafd22ea1c2dee5d6b965b2ef5993046
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-17 09:43:43 +0000

    Wed, May 17, 2023, 11:43 AM +02:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,1 +1,15 @@
-# TODO
+from cs50 import get_int
+
+while True:
+    height = get_int("Height: ")
+    if height < 1 or height > 8:
+        height = get_int("Height: ")
+        if height > 1 or height < 8:
+            break
+    else:
+        break
+
+
+for i in range(1, height + 1):
+    print((' ') * (height - i) + ('#') * (i))
+    i += 1
