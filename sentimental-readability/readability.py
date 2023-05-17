1335bebee19109acc5b11d0a0debe7c92605a04b
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-17 15:18:18 +0000

    Wed, May 17, 2023, 5:18 PM +02:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -4,16 +4,16 @@ from cs50 import get_string
 def count_letters(text):
     letters = 0
     for i in range(len(text)):
-        if(text[i].isalpha()):
+        if (text[i].isalpha()):
             letters += 1
     return letters
 
 
-#count total spaces, then +1 = total amount of words
+# count total spaces, then +1 = total amount of words
 def count_words(text):
     words = 0
-    for i in range (len(text)):
-        if(text[i].isspace()):
+    for i in range(len(text)):
+        if (text[i].isspace()):
             words += 1
     return words + 1
 
@@ -22,22 +22,22 @@ def count_words(text):
 def count_sentences(text):
     sentences = 0
     for i in range(len(text)):
-        if(text[i] == '.' or text[i] == '!' or text[i] == '?'):
+        if (text[i] == '.' or text[i] == '!' or text[i] == '?'):
             sentences += 1
     return sentences
 
 
-#prompt for input
+# prompt for input
 text = get_string("Text: ")
 
 lettersTotal = count_letters(text)
 wordsTotal = count_words(text)
 sentencesTotal = count_sentences(text)
 
-#//use formula to calculate grade
-#//index = 0.0588 * L - 0.296 * S - 15.8
-#//L average number of letters per 100 words - S average number of sentences per 100 words
-#//use floats to calc these """
+# use formula to calculate grade
+# index = 0.0588 * L - 0.296 * S - 15.8
+# L average number of letters per 100 words - S average number of sentences per 100 words
+# use floats to calc these """
 
 grade = 0.0588 * ((lettersTotal / wordsTotal) * 100) - 0.296 * ((sentencesTotal / wordsTotal) * 100) - 15.8
 
