3f112a62c7fd884e8345766e819fdeba763164a3
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-17 14:54:22 +0000

    Wed, May 17, 2023, 4:54 PM +02:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -3,14 +3,16 @@ from cs50 import get_string
 #prompt for input
 text = get_string("Text: ")
 
-   """  //use formula to calculate grade
-    //index = 0.0588 * L - 0.296 * S - 15.8
-    //L average number of letters per 100 words - S average number of sentences per 100 words
-    //use floats to calc these """
+#//use formula to calculate grade
+#//index = 0.0588 * L - 0.296 * S - 15.8
+#//L average number of letters per 100 words - S average number of sentences per 100 words
+#//use floats to calc these """
 
 def count_letters(text):
     letters = 0
     for i in range(len(text)):
         if(i.isalpha()):
             letters += 1
-return letters
+    return letters
+
+print(letters)
