d335c9f5192166eb9f7de6849212d6e35636dc09
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-17 14:44:50 +0000

    Wed, May 17, 2023, 4:44 PM +02:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -7,3 +7,5 @@ text = get_string("Text: ")
     //index = 0.0588 * L - 0.296 * S - 15.8
     //L average number of letters per 100 words - S average number of sentences per 100 words
     //use floats to calc these """
+
+count_letters(text)
