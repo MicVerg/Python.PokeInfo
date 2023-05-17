03f095bfb5ea5d364bb19eb887d469b35105f389
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-17 11:21:09 +0000

    Wed, May 17, 2023, 1:21 PM +02:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,1 +1,11 @@
-# TODO
+from cs50 import get_float
+
+while True:
+    cents = get_float("Change owed: ")
+    if cents < 1:
+        cents = get_float("Change owed: ")
+        if cents > 0:
+            break
+    else:
+        break
+
