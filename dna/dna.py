5016a48d11b88f25c2bc0a5e74b8af568ff7c4c7
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-20 12:34:38 +0000

    Sat, May 20, 2023, 2:34 PM +02:00

--- dna/dna.py
+++ dna/dna.py
@@ -39,9 +39,11 @@ def main():
     # TODO: Check database for matching profiles
     slicedDatabase = [{key: value for key, value in d.items() if key != 'name'} for d in database]
     print(slicedDatabase)
-    
+
     if counts in slicedDatabase:
-        print("succes!")
+        print("success!")
+    else:
+        print("no success!")
     return
 
 
