cd62e51218ea7afe6ef3f9052cfefd46e7ed0f24
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-20 10:05:23 +0000

    Sat, May 20, 2023, 12:05 PM +02:00

--- dna/dna.py
+++ dna/dna.py
@@ -27,6 +27,7 @@ def main():
     # TODO: Find longest match of each STR in DNA sequence
     counts = []
     i = 0
+    slicedDatabase = database[]
     while True:
         longest_match(sequenceReader, database[2])
     agatcCount = longest_match(sequenceReader, "AGATC")
