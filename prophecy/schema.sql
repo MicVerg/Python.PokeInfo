3ff4033f168c62181a382449585692abfae4ef82
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-23 14:28:26 +0000

    Tue, May 23, 2023, 4:28 PM +02:00

--- prophecy/schema.sql
+++ prophecy/schema.sql
@@ -1,5 +1,6 @@
 students
-
+CREATE TABLE students (id INTEGER, student_name TEXT, PRIMARY KEY(id))
 houses
-
+CREATE TABLE houses (id INTEGER, house TEXT, PRIMARY KEY(id))
 house assignments
+CREATE TABLE assignments (id INTEGER, head TEXT, PRIMARY KEY(id))
