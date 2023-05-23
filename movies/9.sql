a209fa433cdfb9796bea28b3aca469da5ca793b6
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-23 18:50:23 +0000

    Tue, May 23, 2023, 8:50 PM +02:00

--- movies/9.sql
+++ movies/9.sql
@@ -1,4 +1,5 @@
-SELECT name FROM people
+SELECT DISTINCT name FROM people
 JOIN stars ON people.id = stars.person_id
 JOIN movies ON movies.year = 2004
-ORDER BY people.birth;
+ORDER BY people.birth
+LIMIT 10;
