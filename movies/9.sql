f892c4e2e88b09059784fe7c2f5225aa5c88f1f4
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-23 18:54:36 +0000

    Tue, May 23, 2023, 8:54 PM +02:00

--- movies/9.sql
+++ movies/9.sql
@@ -1,5 +1,4 @@
 SELECT DISTINCT name FROM people
 JOIN stars ON people.id = stars.person_id
 WHERE stars.movie_id IN (SELECT id FROM movies WHERE year = 2004)
-ORDER BY people.birth ASC
-LIMIT 10;
+ORDER BY people.birth ASC;
