ae25ed2d0e09d6cbe405680a72aa02f2dec3cbf3
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-23 20:31:03 +0000

    Tue, May 23, 2023, 10:31 PM +02:00

--- movies/12.sql
+++ movies/12.sql
@@ -4,4 +4,4 @@ WHERE stars.person_id = (SELECT id FROM people WHERE name = 'Johnny Depp')
 AND title IN
 (SELECT title FROM movies
 JOIN stars on movies.id = stars.movie_id
-WHERE stars.person_id = SELECT id FROM people WHERE name = 'Helena Bonham Carter');
+WHERE stars.person_id IN SELECT id FROM people WHERE name = 'Helena Bonham Carter');
