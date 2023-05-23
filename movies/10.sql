f7d8d36f4c8e349270f3d92d68706060a344935f
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-23 19:04:36 +0000

    Tue, May 23, 2023, 9:04 PM +02:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,1 +1,3 @@
-(SELECT movie_id FROM ratings WHERE rating >= 9.0)
+SELECT DISTINCT name FROM people
+JOIN directors ON people.id = directors.person_id
+WHERE directors.movie_id IN (SELECT movie_id FROM ratings WHERE rating >= 9.0);
