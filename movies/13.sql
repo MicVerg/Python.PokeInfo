0d673c784ee7f183a4a7a04194425535edcea0ca
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-23 20:11:44 +0000

    Tue, May 23, 2023, 10:11 PM +02:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,4 +1,4 @@
 SELECT name FROM people
 JOIN stars ON people.id = stars.person_id
-WHERE id IN 
-SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = 'Kevin Bacon' AND (SELECT id FROM people WHERE birth = 1958));
+WHERE id IN
+(SELECT person_id FROM stars WHERE movie_id IN (SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = 'Kevin Bacon' AND (SELECT id FROM people WHERE birth = 1958))));
