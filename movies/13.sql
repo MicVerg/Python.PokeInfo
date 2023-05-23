37b8a6c60e15f7ee161f03788ae1513e14981fd2
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-23 19:55:55 +0000

    Tue, May 23, 2023, 9:55 PM +02:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,3 +1,3 @@
 SELECT name FROM people
 JOIN stars ON people.id = stars.person_id
-SELECT movie_id FROM stars = (SELECT id FROM people WHERE name = 'Kevin Bacon' AND birth = 1958);
+SELECT movie_id FROM stars WHERE people.id = (SELECT id FROM people WHERE name = 'Kevin Bacon');
