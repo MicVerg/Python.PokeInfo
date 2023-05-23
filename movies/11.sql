b18e951fd2b6cded409f7f100023b51b1b6cd5ff
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-23 19:13:19 +0000

    Tue, May 23, 2023, 9:13 PM +02:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,4 +1,6 @@
 SELECT title FROM movies
 JOIN ratings ON movies.id = ratings.movie_id
-
-(SELECT id FROM people WHERE name = 'Chadwick Boseman')
+JOIN stars on movies.id = stars.movie_id
+WHERE stars.person_id = (SELECT id FROM people WHERE name = 'Chadwick Boseman')
+ORDER BY ratings.rating DESC
+LIMIT 5;
