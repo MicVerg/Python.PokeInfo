e506a31df5ebc47c221f3247de520278ea66f272
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-23 19:36:46 +0000

    Tue, May 23, 2023, 9:36 PM +02:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,3 +1,13 @@
-SELECT title FROM movies
-JOIN stars on movies.id = stars.movie_id
-WHERE stars.person_id IN (SELECT id FROM people WHERE name = 'Johnny Depp' INTERSECT SELECT id FROM people WHERE name = 'Helena Bonham Carter');
+SELECT title
+FROM movies
+WHERE id IN (
+    SELECT movie_id
+    FROM stars
+    WHERE person_id IN (
+        SELECT id
+        FROM people
+        WHERE name IN ('Johnny Depp', 'Helena Bonham Carter')
+    )
+    GROUP BY movie_id
+    HAVING COUNT(DISTINCT person_id) = 2
+);
