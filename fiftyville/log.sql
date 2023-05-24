5c65b0647d89b7e2937d39939fc84f8efe3f7289
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-24 12:31:02 +0000

    Wed, May 24, 2023, 2:31 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -47,7 +47,7 @@ SELECT person_id FROM bank_accounts
 -- check combo of person IDs and license plates, these are the names of people who were at bakery + withdrew SUSPECT LIST
 SELECT DISTINCT name FROM people
     JOIN bakery_security_logs ON people.license_plate = bakery_security_logs.license_plate
-    WHERE people.id IN
+    WHERE people.id =
         (SELECT person_id FROM bank_accounts
         JOIN atm_transactions ON bank_accounts.account_number = atm_transactions.account_number
         WHERE bank_accounts.account_number IN
