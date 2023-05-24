3787834fd9860c772ffeaa29607f4c3219cef9f0
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-24 07:31:54 +0000

    Wed, May 24, 2023, 9:31 AM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -33,7 +33,7 @@ SELECT account_number
     AND transaction_type = 'withdraw'
     AND atm_location = 'Leggett Street';
 
--- check person_id based on ATM withdrawals
+-- check person_id based on ATM withdrawals of theft day
 SELECT person_id FROM bank_accounts
     JOIN atm_transactions ON bank_accounts.account_number = atm_transactions.account_number
     WHERE bank_accounts.account_number IN
