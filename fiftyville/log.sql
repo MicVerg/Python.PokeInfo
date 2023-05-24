da79869147dd1e37bdbe3755b356b7b52f66b2aa
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-24 07:30:39 +0000

    Wed, May 24, 2023, 9:30 AM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -32,3 +32,14 @@ SELECT account_number
     AND year = 2021
     AND transaction_type = 'withdraw'
     AND atm_location = 'Leggett Street';
+
+-- check person_id based on ATM withdrawals
+SELECT person_id FROM bank_accounts
+    JOIN atm_transactions ON bank_accounts.account_number = atm_transactions.account_number
+    WHERE bank_accounts.account_number IN
+        (SELECT account_number FROM atm_transactions
+        WHERE DAY = 28
+        AND MONTH = 7
+        AND year = 2021
+        AND transaction_type = 'withdraw'
+        AND atm_location = 'Leggett Street');
