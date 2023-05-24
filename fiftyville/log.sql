aa4a84ef725026c021341e70f2863813b5e3dd6e
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-24 06:34:29 +0000

    Wed, May 24, 2023, 8:34 AM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,4 +1,7 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
-SELECT *
+SELECT description
     FROM crime_scene_reports
-    
+    WHERE street IN 'Humphrey Street'
+    AND day IN 28
+    AND month IN 7
+    AND year IN 2021;
