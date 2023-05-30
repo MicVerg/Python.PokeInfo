daf5ab4406def4d79d2a12a3258051504566f3be
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-30 13:46:56 +0000

    Tue, May 30, 2023, 3:46 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -84,7 +84,7 @@ def buy():
     if currentCash >= transactionCost:
         db.execute("INSERT INTO transactions (user_id, symbol, shares, price, timestamp) VALUES (?, ?, ?, ?, ?)", user_id, symbol, shares, quote, current_timestamp)
         newCash = currentCash - transactionCost
-        db.execute("UPDATE users (user_id, cash) VALUES (?, ?)", user_id, newCash)
+        db.execute("UPDATE users SET cash = ? WHERE user_id = ?", newCash, user_id)
     else:
         return apology("You ain't got no money")
 
