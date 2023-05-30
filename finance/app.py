5b8eaca78e2570b418f33452e92c3472c266c9d4
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-30 13:25:27 +0000

    Tue, May 30, 2023, 3:25 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -73,7 +73,8 @@ def buy():
 
     # when POST buy the stock if user has enough money, if not = apology
     user_id = session["user_id"]
-    currentCash = db.execute("SELECT cash FROM users WHERE id = ?", id-user_id)
+    currentCash = db.execute("SELECT cash FROM users WHERE id = ?", user_id)
+    
     # if yes, run SQL on db to purchase stock
     # update portfolio and update cash
     return render_template("/")
