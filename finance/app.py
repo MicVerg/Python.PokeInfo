2752495c0888ac9e165632fb1ed9dc95893ea826
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-30 07:04:50 +0000

    Tue, May 30, 2023, 9:04 AM +02:00

--- finance/app.py
+++ finance/app.py
@@ -146,7 +146,6 @@ def register():
         elif not request.form.get("confirmpassword"):
             return apology("Must confirm password", 403)
 
-        elif
     # once confirmed, generate_password_hash and add that hash to the database
     # db.execute to add to table
 
