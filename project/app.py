c4e8c7a24047877a08e6c3255b5f963ad5e8fa39
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-31 15:13:07 +0000

    Wed, May 31, 2023, 5:13 PM +02:00

--- project/app.py
+++ project/app.py
@@ -11,3 +11,14 @@ from datetime import datetime
 # Configure application
 app = Flask(__name__)
 
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///project.db")
+
+@app.route("/")
+def index():
+    return render_template("index.html")
