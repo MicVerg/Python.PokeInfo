5aaa1029beb3032861fb5bc7f92f1d6f6657fdbf
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-31 15:10:06 +0000

    Wed, May 31, 2023, 5:10 PM +02:00

--- project/app.py
+++ project/app.py
@@ -7,7 +7,6 @@ from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
 
-from helpers import apology, login_required, lookup, usd
 
 # Configure application
 app = Flask(__name__)
