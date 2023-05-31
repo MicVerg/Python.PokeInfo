592d3b72ecef9c3770aa60ca264ee918f934d262
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-05-31 14:55:57 +0000

    Wed, May 31, 2023, 4:55 PM +02:00

---
+++ project/app.py
@@ -0,0 +1,14 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
+from datetime import datetime
+
+from helpers import apology, login_required, lookup, usd
+
+# Configure application
+app = Flask(__name__)
+
