3eac6cc7527079ac2d1c82fc4858f829a39f9fa0
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-01 15:02:59 +0000

    Thu, Jun 1, 2023, 5:02 PM +02:00

--- project/database.py
+++ project/database.py
@@ -8,7 +8,7 @@ from flask import redirect, render_template, request, session
 from functools import wraps
 
 url = "https://pokeapi.co/api/v2/pokemon/"
-params = {"id": "1"}
+params = {"id": "151"}
 
 response = requests.get(url, params)
 
