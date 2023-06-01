1fdecb30da7ea9dd26df0df27d312fb4a0f8b3a3
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-01 13:02:11 +0000

    Thu, Jun 1, 2023, 3:02 PM +02:00

--- project/database.py
+++ project/database.py
@@ -6,7 +6,3 @@ from bs4 import BeautifulSoup
 from flask import redirect, render_template, request, session
 from functools import wraps
 
-html_text = requests.get('https://www.ictjob.be/nl/it-vacatures-zoeken?skills=411&locations=6').text
-soup = BeautifulSoup(html_text, 'lxml')
-jobs = soup.find_all('li', class_ = 'search-item.clearfix')
-print(jobs)
