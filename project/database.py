b4e824710a4d058c355c444b144ba2d5d31f833f
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-01 12:14:03 +0000

    Thu, Jun 1, 2023, 2:14 PM +02:00

--- project/database.py
+++ project/database.py
@@ -6,10 +6,4 @@ from bs4 import BeautifulSoup
 from flask import redirect, render_template, request, session
 from functools import wraps
 
-with open('Sandbox.html', 'r') as html_file:
-    content = html_file.read()
-
-    soup = BeautifulSoup(content, 'lxml')
-    tags = soup.find_all('h3')
-    for tag in tags:
-        print(tag.text)
+html_text = requests.get('')
