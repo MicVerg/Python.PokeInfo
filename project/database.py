849262dfe8244cc976c96a95389b7561060589d1
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-01 14:32:35 +0000

    Thu, Jun 1, 2023, 4:32 PM +02:00

--- project/database.py
+++ project/database.py
@@ -6,3 +6,7 @@ from bs4 import BeautifulSoup
 from flask import redirect, render_template, request, session
 from functools import wraps
 
+url = "https://pokeapi.co/api/v2/pokemon/"
+params = {"Squirtle"}
+
+response = requests.get(url, params)
