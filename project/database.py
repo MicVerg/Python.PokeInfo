8fe08bc27b4b2c398e4da936c89ca3c42686f280
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-03 05:32:33 +0000

    Sat, Jun 3, 2023, 7:32 AM +02:00

--- project/database.py
+++ project/database.py
@@ -8,13 +8,18 @@ from bs4 import BeautifulSoup
 from flask import redirect, render_template, request, session
 from functools import wraps
 
-url = "https://pokeapi.co/api/v2/pokemon/"
-params = {"id": "10"}
+pokeString = input("Enter a pokemon name or id: ")
+url = "https://pokeapi.co/api/v2/pokemon/" + pokeString
+# params = {"id": "10"}
 
-response = requests.get(url, params=params)
-print(response.url)
+
+response = requests.get(url)
+
 if response.status_code == 200:
     data = json.loads(response.text)
+    pokemonName = data['types'][0]['type']['name']
     pprint.pprint(data)
+    print(response.url)
+    print("That pokemon is " + pokemonName)
 else:
     print(f"Error: {reponse.status_code}")
