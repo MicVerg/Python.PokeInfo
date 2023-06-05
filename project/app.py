a711d94ba7597038441feb06886d025850586b52
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-05 09:15:00 +0000

    Mon, Jun 5, 2023, 11:15 AM +02:00

--- project/app.py
+++ project/app.py
@@ -41,7 +41,7 @@ def pokedex():
             img = data['sprites']['front_default']
             name = (data['forms'][0]['name']).capitalize()
             type = (data['types'][0]['type']['name']).capitalize()
-            pokeID = data['game_indices'][0]['game_index']
+            pokeID = data['id']
             return render_template("result.html", nameID=nameID, url=url, img=img, name=name, type=type, pokeID=pokeID)
         else: flash(f"Error: {response.status_code}", "error")
 
