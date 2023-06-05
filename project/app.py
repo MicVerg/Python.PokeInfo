3c4e28a5cd5ab4e8f285321a767f79d8ac620543
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-05 13:00:54 +0000

    Mon, Jun 5, 2023, 3:00 PM +02:00

--- project/app.py
+++ project/app.py
@@ -79,6 +79,11 @@ def pokedex():
                     if entry['language']['name'] == 'en' and entry['version']['name'] == 'sun':
                         flavor_text = entry['flavor_text']
                         break
+            if pokeID >= 810 <= 905:
+                for entry in pokeSpecies['flavor_text_entries']:
+                    if entry['language']['name'] == 'en' and entry['version']['name'] == 'sword':
+                        flavor_text = entry['flavor_text']
+                        break
             flavor_text = flavor_text.replace('\u000c', ' ')
             return render_template("result.html", nameID=nameID, url=url, img=img, name=name, type=type, pokeID=pokeID, height=height, weight=weight, flavor_text=flavor_text)
         else:
