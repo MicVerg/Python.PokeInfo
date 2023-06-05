91ae24cc17bcc9234ad00eb61aa92ac1ca9148a2
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-05 13:18:37 +0000

    Mon, Jun 5, 2023, 3:18 PM +02:00

--- project/app.py
+++ project/app.py
@@ -91,6 +91,7 @@ def pokedex():
                         break
             # fix u000c
             flavor_text = flavor_text.replace('\u000c', ' ')
+            flavor_text = flavor_text.replace('\u00A0', '')
             return render_template("result.html", nameID=nameID, url=url, img=img, name=name, type=type, pokeID=pokeID, height=height, weight=weight, flavor_text=flavor_text)
         else:
             flash("Pokemon name or ID not found, please try again.")
