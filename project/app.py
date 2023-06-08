edb00997444d2d507357c5706a5b3b8838297a09
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-08 13:32:17 +0000

    Thu, Jun 8, 2023, 3:32 PM +02:00

--- project/app.py
+++ project/app.py
@@ -224,8 +224,6 @@ def pokedex():
             else:
                 evolutionImg = "/static/icons8-no-entry-80.png"
             return render_template("result.html", nameID=nameID, url=url, img=img, name=name, type=type, pokeID=pokeID, height=height, weight=weight, flavor_text=flavor_text, pokeEvolution=pokeEvolution, evolutionImg=evolutionImg, evolutionName=evolutionName, evolutionID=evolutionID)
-
-
         else:
             flash("Pokemon name or ID not found, please try again.")
             return redirect("/pokedex")
