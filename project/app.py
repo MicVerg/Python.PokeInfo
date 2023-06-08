64eafb57dd08fd1c4128e2d882994906501032eb
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-08 11:40:39 +0000

    Thu, Jun 8, 2023, 1:40 PM +02:00

--- project/app.py
+++ project/app.py
@@ -25,7 +25,102 @@ def index():
 @app.route("/pokedex", methods=["GET", "POST"])
 def pokedex():
     if request.method == "GET":
-        return render_template("pokedex.html")
+        nameID = request.form.get("nameID")
+        url = "https://pokeapi.co/api/v2/pokemon/" + nameID
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = json.loads(response.text)
+            img = data['sprites']['front_default']
+            name = (data['forms'][0]['name'])
+            type = (data['types'][0]['type']['name'])
+            pokeID = data['id']
+            height = data['height']
+            weight = data['weight']
+
+            # flavor text
+            pokeSpecies = json.loads(requests.get("https://pokeapi.co/api/v2/pokemon-species/" + nameID).text)
+            flavor_text = ""
+            if pokeID >= 1 <= 151:
+                for entry in pokeSpecies['flavor_text_entries']:
+                    if entry['language']['name'] == 'en' and entry['version']['name'] == 'blue':
+                        flavor_text = entry['flavor_text']
+                        break
+            if pokeID >= 152 <= 251:
+                for entry in pokeSpecies['flavor_text_entries']:
+                    if entry['language']['name'] == 'en' and entry['version']['name'] == 'silver':
+                        flavor_text = entry['flavor_text']
+                        break
+            if pokeID >= 252 <= 386:
+                for entry in pokeSpecies['flavor_text_entries']:
+                    if entry['language']['name'] == 'en' and entry['version']['name'] == 'ruby':
+                        flavor_text = entry['flavor_text']
+                        break
+            if pokeID >= 387 <= 493:
+                for entry in pokeSpecies['flavor_text_entries']:
+                    if entry['language']['name'] == 'en' and entry['version']['name'] == 'diamond':
+                        flavor_text = entry['flavor_text']
+                        break
+            if pokeID >= 494 <= 649:
+                for entry in pokeSpecies['flavor_text_entries']:
+                    if entry['language']['name'] == 'en' and entry['version']['name'] == 'black':
+                        flavor_text = entry['flavor_text']
+                        break
+            if pokeID >= 650 <= 721:
+                for entry in pokeSpecies['flavor_text_entries']:
+                    if entry['language']['name'] == 'en' and entry['version']['name'] == 'x':
+                        flavor_text = entry['flavor_text']
+                        break
+            if pokeID >= 722 <= 809:
+                for entry in pokeSpecies['flavor_text_entries']:
+                    if entry['language']['name'] == 'en' and entry['version']['name'] == 'sun':
+                        flavor_text = entry['flavor_text']
+                        break
+            if pokeID >= 810 <= 899:
+                for entry in pokeSpecies['flavor_text_entries']:
+                    if entry['language']['name'] == 'en' and entry['version']['name'] == 'sword':
+                        flavor_text = entry['flavor_text']
+                        break
+            if pokeID >= 899 <= 905:
+                for entry in pokeSpecies['flavor_text_entries']:
+                    if entry['language']['name'] == 'en' and entry['version']['name'] == 'legends-arceus':
+                        flavor_text = entry['flavor_text']
+                        break
+            # fix u000c
+            flavor_text = flavor_text.replace('\u000c', ' ')
+
+            # evolves into
+            pokeEvolution = ""
+            evolutionName = ""
+            evolutionImg = "/static/icons8-no-entry-80.png"
+            url = pokeSpecies['evolution_chain']['url']
+            evolutionChain = json.loads(requests.get(url).text)['chain']
+            if 'evolves_to' in evolutionChain and name == evolutionChain['species']['name']:
+                try:
+                    first_evolution = evolutionChain['evolves_to'][0]
+                    pokeEvolution = first_evolution['species']['url']
+                    evolutionResponse = requests.get(pokeEvolution)
+                    evolutionData = json.loads(evolutionResponse.text)
+                    evolutionID = evolutionData['id']
+                    evolutionImg = (json.loads((requests.get("https://pokeapi.co/api/v2/pokemon/" + str(evolutionID))).text))['sprites']['front_default']
+                    evolutionName = (json.loads((requests.get("https://pokeapi.co/api/v2/pokemon/" + str(evolutionID))).text))['forms'][0]['name']
+                except IndexError:
+                    second_evolution = None
+
+            elif 'evolves_to' in evolutionChain and name == evolutionChain['evolves_to'][0]['species']['name']:
+                try:
+                    second_evolution = evolutionChain['evolves_to'][0]['evolves_to'][0]
+                    pokeEvolution = second_evolution['species']['url']
+                    evolutionResponse = requests.get(pokeEvolution)
+                    evolutionData = json.loads(evolutionResponse.text)
+                    evolutionID = evolutionData['id']
+                    evolutionImg = (json.loads((requests.get("https://pokeapi.co/api/v2/pokemon/" + str(evolutionID))).text))['sprites']['front_default']
+                    evolutionName = (json.loads((requests.get("https://pokeapi.co/api/v2/pokemon/" + str(evolutionID))).text))['forms'][0]['name']
+                except IndexError:
+                    second_evolution = None
+
+            else:
+                evolutionImg = "/static/icons8-no-entry-80.png"
+            return render_template("result.html", nameID=nameID, url=url, img=img, name=name, type=type, pokeID=pokeID, height=height, weight=weight, flavor_text=flavor_text, pokeEvolution=pokeEvolution, evolutionImg=evolutionImg, evolutionName=evolutionName, evolutionID=evolutionID)
 
     elif request.method == "POST":
         nameID = request.form.get("nameID")
