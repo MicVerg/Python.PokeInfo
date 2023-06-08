f41bc12aad73e9c841d23fdc8e6587b1d2c61ed9
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-08 11:27:37 +0000

    Thu, Jun 8, 2023, 1:27 PM +02:00

--- project/app.py
+++ project/app.py
@@ -25,7 +25,7 @@ def index():
 @app.route("/pokedex", methods=["GET", "POST"])
 def pokedex():
     if request.method == "GET":
-        return render_template("result.html")
+        return render_template("result.html", nameID=nameID, url=url, img=img, name=name, type=type, pokeID=pokeID, height=height, weight=weight, flavor_text=flavor_text, pokeEvolution=pokeEvolution, evolutionImg=evolutionImg, evolutionName=evolutionName)
 
     elif request.method == "POST":
         nameID = request.form.get("nameID")
