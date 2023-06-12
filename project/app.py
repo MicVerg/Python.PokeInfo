27ba997d1f56d5d3716958108517572c622902bb
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-12 08:21:17 +0000

    Mon, Jun 12, 2023, 10:21 AM +02:00

--- project/app.py
+++ project/app.py
@@ -229,7 +229,9 @@ def pokedex():
             # evolves from
             pokeEvolutionFrom, evolutionFromName, evolutionFromID = "", "", ""
             evolutionFromName = (json.loads((requests.get("https://pokeapi.co/api/v2/pokemon-species/" + pokeID)).text))
-            if 'evolves_from_species' in evolutionFromName and evolutionFromName['evolves_from_species'] != "null"
+            if 'evolves_from_species' in evolutionFromName and evolutionFromName['evolves_from_species'] != "null":
+                
+
 
             else:
                 evolutionImg = "/static/icons8-no-entry-80.png"
