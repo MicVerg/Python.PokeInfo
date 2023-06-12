2542cd09da6ff6d1348a2ad0d9eb009b4ca4a9bb
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-12 14:17:23 +0000

    Mon, Jun 12, 2023, 4:17 PM +02:00

--- project/app.py
+++ project/app.py
@@ -88,7 +88,7 @@ def pokedex():
 
                 # fix flavor text
                 flavor_text = flavor_text.replace('\u000c', ' ')
-                flavor_text = flavor_text.replace("&shy;", "").replace(";\xad", "").replace("\xad", "").replace("\n", "")
+                flavor_text = flavor_text.replace("&shy;", "").replace(";\xad", "").replace("\xad", "").replace("\n", " ").replace("-", " - ")
 
                 # evolves into
                 pokeEvolution, evolutionName, evolutionID = "", "", ""
@@ -206,7 +206,7 @@ def pokedex():
 
             # fix flavor text
             flavor_text = flavor_text.replace('\u000c', ' ')
-            flavor_text = flavor_text.replace("&shy;", "").replace(";\xad", "").replace("\xad", "").replace("\n", "")
+            flavor_text = flavor_text.replace("&shy;", "").replace(";\xad", "").replace("\xad", "").replace("\n", " ").replace("-", " - ")
 
             # evolves into
             pokeEvolution, evolutionName, evolutionID = "", "", ""
