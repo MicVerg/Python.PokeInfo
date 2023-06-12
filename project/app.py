c209c13985294b6a5fc856c2b75493f8ccc1551d
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-12 08:09:00 +0000

    Mon, Jun 12, 2023, 10:09 AM +02:00

--- project/app.py
+++ project/app.py
@@ -227,7 +227,7 @@ def pokedex():
                     second_evolution = None
 
              # evolves from
-             
+             pokeEvolutionFrom, evolutionFromName, evolutionFromID = "", "", ""
             else:
                 evolutionImg = "/static/icons8-no-entry-80.png"
             return render_template("result.html", nameID=nameID, url=url, img=img, name=name, type=type, pokeID=pokeID, height=height, weight=weight, flavor_text=flavor_text, pokeEvolution=pokeEvolution, evolutionImg=evolutionImg, evolutionName=evolutionName, evolutionID=evolutionID)
