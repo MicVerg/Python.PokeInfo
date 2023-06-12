85e603b0b78f8f67f1e670d4e046188ec19e2f70
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-12 13:35:06 +0000

    Mon, Jun 12, 2023, 3:35 PM +02:00

--- project/app.py
+++ project/app.py
@@ -1,22 +1,12 @@
-import os
 import requests
 import json
 
-from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 
 
 # Configure application
 app = Flask(__name__)
-
-# Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
-
-# Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///project.db")
 
 @app.route("/")
 def index():
