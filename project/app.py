30f449dda4028dcf5a1d57b7fb759394142ef3b6
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-12 14:31:50 +0000

    Mon, Jun 12, 2023, 4:31 PM +02:00

--- project/app.py
+++ project/app.py
@@ -207,7 +207,7 @@ def pokedex():
 
             # fix flavor text
             flavor_text = flavor_text.replace('\u000c', ' ')
-            
+            flavor_text = flavor_text.replace("&shy", '').replace(";\xad","").replace("\xad","")
 
 
             # evolves into
