da15112f15b34131bd4960051f904ae9603e0cc4
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-13 09:27:30 +0000

    Tue, Jun 13, 2023, 11:27 AM +02:00

--- faces/faces.py
+++ faces/faces.py
@@ -1,11 +1,10 @@
 def convert(text):
     text.replace(":)", "🙂").replace(":(","🙁")
-    return text
+    print(text)
 
 def main():
     text=input()
     convert(text)
     print(text)
 
-if __name__ == "__main__":
-    main()
+main()
