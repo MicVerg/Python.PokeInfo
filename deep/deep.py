39ef196b66bd246a20b40921b3327ce2c5d256a6
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-14 08:30:11 +0000

    Wed, Jun 14, 2023, 10:30 AM +02:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,5 +1,5 @@
 input = input("What is the answer to the Great Question of Life, the Universe and Everything? ")
-if input == "42":
+if input.strip(' ') == "42":
     print("Yes")
 elif input.lower() == "forty-two":
     print("Yes")
