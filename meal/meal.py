e9f991228a64819cb610f3b2bbb4be155938931c
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-14 09:40:15 +0000

    Wed, Jun 14, 2023, 11:40 AM +02:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,10 +1,13 @@
 def main():
     time = input("What time is it? ")
-
+    convert(time)
 
 def convert(time):
-    
-
+    hours, minutes = time.split(':')
+    hours = float(hours)
+    minutes = float(minutes) / 60
+    result = hours + minutes
+    return result
 
 if __name__ == "__main__":
     main()
