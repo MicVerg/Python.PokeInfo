084fb135c8d9e292e41d9a45e959cc93bc262ab9
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-14 09:00:28 +0000

    Wed, Jun 14, 2023, 11:00 AM +02:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,4 +1,16 @@
 text = input("File name: ")
 text = text.strip(' ').lower()
-splitText = text.split('.')
-if text ==
+splitText = text.split('.')[1]
+
+if splitText == "gif":
+    print("image/gif")
+elif splitText == "jpg":
+    print("image/jpeg")
+elif splitText == "png":
+    print("image/png")
+elif splitText == "pdf":
+    print("application/pdf")
+elif splitText == "txt":
+    print("text/plain")
+elif splitText == "zip":
+    print("application/zip")
