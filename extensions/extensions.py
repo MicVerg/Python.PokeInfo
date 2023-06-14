d17fa468e009c5dc66300e01047e8b311fe1fb51
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-14 09:01:42 +0000

    Wed, Jun 14, 2023, 11:01 AM +02:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -6,6 +6,8 @@ if splitText == "gif":
     print("image/gif")
 elif splitText == "jpg":
     print("image/jpeg")
+elif splitText == "jpeg":
+    print("image/jpeg")
 elif splitText == "png":
     print("image/png")
 elif splitText == "pdf":
@@ -14,3 +16,5 @@ elif splitText == "txt":
     print("text/plain")
 elif splitText == "zip":
     print("application/zip")
+else:
+    print("application/octet-stream")
