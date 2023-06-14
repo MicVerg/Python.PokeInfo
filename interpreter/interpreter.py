8a673a2101a20471c6a387d0aab7e4167a18d454
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-14 09:14:23 +0000

    Wed, Jun 14, 2023, 11:14 AM +02:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,5 +1,5 @@
 input = input("Expression: ")
 inputSplit = input.split(' ')
 
-result = float(inputSplit[0]) + inputSplit[1] + float(inputSplit[2])
+result = float(inputSplit[0]) + float(inputSplit[1]) + float(inputSplit[2])
 print(result)
