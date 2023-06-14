61c9524526a473dffe0e6b23958a26f63ef48d8f
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-14 15:34:35 +0000

    Wed, Jun 14, 2023, 5:34 PM +02:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -3,6 +3,7 @@ while True:
     try:
         user_input = input("Fraction: ")
         x, blergh, y = user_input.split('/')
+        print(x, y)
     except (ValueError, ZeroDivisionError):
         pass
     else:
