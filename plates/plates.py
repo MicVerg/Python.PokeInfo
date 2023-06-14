7d0cb4a0d544164ad723c2e73e23f33e760e462d
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-14 13:38:33 +0000

    Wed, Jun 14, 2023, 3:38 PM +02:00

--- plates/plates.py
+++ plates/plates.py
@@ -8,9 +8,13 @@ def main():
 
 def is_valid(s):
     two_letters = s[0:2].isalpha()
-    if two_letters = False:
+    if two_letters == False:
+        return False
+    plate_length = len(s)
+    if plate_length < 2 or plate_length > 6:
         return False
     
+
 
 
 main()
