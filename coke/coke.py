a937e8f50ca2d527269b422a997fbe2518915326
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-14 12:25:46 +0000

    Wed, Jun 14, 2023, 2:25 PM +02:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,6 +1,11 @@
-change = ""
-paid = ""
+due = 0
+paid = 0
 
 while True:
     input = input("Insert Coin: ")
-    
+    paid += int(input)
+    due = 50 - paid
+    print("Amount Due: " + str(due))
+
+    if due <= 0:
+        break
