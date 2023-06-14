10254e7c19b5e0dc4ecce0a580c28ae1435c2b2f
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-14 12:38:50 +0000

    Wed, Jun 14, 2023, 2:38 PM +02:00

--- coke/coke.py
+++ coke/coke.py
@@ -4,13 +4,11 @@ paid = 0
 print("Amount Due: 50")
 
 while True:
-    while True:
-        user_input = input("Insert Coin: ")
-        if user_input == "50" or user_input == "25" or user_input == "10" or user_input == "5":
-            continue
+    user_input = input("Insert Coin: ")
+    if user_input == "50" or user_input == "25" or user_input == "10" or user_input == "5":
         paid += int(user_input)
         due = 50 - paid
         print("Amount Due: " + str(due))
 
-        if due <= 0:
-            break
+    if due <= 0:
+        break
