6d9f57b541d475bb8da3f1c5bc2954df842a1aa6
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-15 07:38:57 +0000

    Thu, Jun 15, 2023, 9:38 AM +02:00

---
+++ outdated/outdated.py
@@ -0,0 +1,18 @@
+months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+# change from MM DD YYYY to YYYY MM DD
+# examples: 12/31/1636 OR december 31, 1636
+
