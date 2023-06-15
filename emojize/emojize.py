1ed92a45765f35f1b1683700c6c69a8999043f93
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-15 15:10:09 +0000

    Thu, Jun 15, 2023, 5:10 PM +02:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -1,4 +1,4 @@
 import emoji
 
 user_input = input("Input: ")
-print(f"emoji.emojize("Python is " + {user_input}", language='alias'))
+print(f"emoji.emojize("Python is " + {user_input}", language='alias')
