598cc0ed3c5fa2fb9510c77ebd1af73abe85c599
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-15 07:59:32 +0000

    Thu, Jun 15, 2023, 9:59 AM +02:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -13,10 +13,12 @@ months = [
     "December"
 ]
 
+while True:
+
 # change from MM/DD/YYYY to YYYY-MM-DD
 # examples: 12/31/1636 OR december 31, 1636
 
-user_input = input("Date: ")
+    user_input = input("Date: ")
 
-M, D, Y = user_input.split('/')
-print(f"{Y}-{int(M):02d}-{int(D):02d}")
+    M, D, Y = user_input.split('/')
+    print(f"{Y}-{int(M):02d}-{int(D):02d}")
