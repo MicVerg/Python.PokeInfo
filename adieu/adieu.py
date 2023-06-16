07c41e7c1878e0a26383fed2fddb5275aef80290
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-16 11:26:19 +0000

    Fri, Jun 16, 2023, 1:26 PM +02:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -8,17 +8,8 @@ while True:
     try:
         user_input = input("Name: ")
         names.append(user_input)
-        names = p.join(names)
 
     except EOFError:
+        names = p.join(names)
         print("Adieu, adieu, to " + names)
         sys.exit()
-
-        if len(names) == 1:
-            print("Adieu, adieu, to " + names)
-            sys.exit()
-        elif len(names) == 2:
-            print("Adieu, adieu, to " + names + ' and ' + names)
-            sys.exit()
-        elif len(names) > 2:
-            print("Adieu, adieu, to " + names)
