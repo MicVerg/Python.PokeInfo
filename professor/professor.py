ddcbe03abcf0fc0bb6b81f717b816bd6f84155bc
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-16 13:54:35 +0000

    Fri, Jun 16, 2023, 3:54 PM +02:00

--- professor/professor.py
+++ professor/professor.py
@@ -44,7 +44,7 @@ def generate_integer(level):
     # return a non-negative integer with LEVEL digits or raise a ValueError if level is not 1 2 or 3
     randy = 0
     if level == 1:
-        randy = random.randint(1, 9)
+        randy = random.randint(0, 9)
     elif level == 2:
         randy = random.randint(10, 99)
     elif level == 3:
