a6e16f82558be6f56d7dc9f19f4ad761c9253434
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-16 11:59:16 +0000

    Fri, Jun 16, 2023, 1:59 PM +02:00

--- game/game.py
+++ game/game.py
@@ -2,7 +2,10 @@ import random
 import sys
 
 level = input("Level: ")
-randy = random.randint(1, int(level))
+while True:
+    if not level.isdigit() or int(level) < 1:
+    randy = random.randint(1, int(level))
+
 
 while True:
     if int(level) < 1 or level.isalpha():
