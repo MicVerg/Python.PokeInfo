b925abff4ff52fe8905b229488d27b619eee21a6
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-16 11:37:59 +0000

    Fri, Jun 16, 2023, 1:37 PM +02:00

--- game/game.py
+++ game/game.py
@@ -1,7 +1,19 @@
 import random
+import sys
 
 while True:
     level = input("Level: ")
     if int(level) < 1:
         level = input("Level: ")
-    
+
+    randy = random.randint(1, level)
+    guess = input("Guess: ")
+        if guess < randy:
+            print("Too small!")
+            guess = input("Guess: ")
+        elif guess > randy:
+            print("Too large!")
+            guess = input("Guess: ")
+        elif guess == randy:
+            print("Just right!")
+            sys.exit()
