12d5764a16e7808f19b7203339c8b87981703b4d
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-19 14:01:49 +0000

    Mon, Jun 19, 2023, 4:01 PM +02:00

--- test_twttr/twttr.py
+++ test_twttr/twttr.py
@@ -1,12 +1,11 @@
 def main():
-    user_input = input("Input: ")
-
-
+    word = input("Input: ")
+    shorten(word)
 
 
 def shorten(word):
     for char in word:
-    word = word.replace('A', '').replace('a', '').replace('E', '').replace('e', '').replace('I', '').replace('i', '').replace('O', '').replace('o', '').replace('U', '').replace('u', '')
+        word = word.replace('A', '').replace('a', '').replace('E', '').replace('e', '').replace('I', '').replace('i', '').replace('O', '').replace('o', '').replace('U', '').replace('u', '')
     return word
 
 if __name__ == "__main__":
