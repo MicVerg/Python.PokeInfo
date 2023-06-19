304a53a461d43c6be8b9156eb6011217b92dcdbd
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-19 14:59:22 +0000

    Mon, Jun 19, 2023, 4:59 PM +02:00

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -2,9 +2,10 @@ from bank import value
 
 
 def test_hello():
-    assert value("hello") == "$100"
+    assert value("hello") == "$0"
 
 def test_ha():
     assert value("ha") == "$20"
 
-def 
+def test_nothing():
+    assert value("no text here") == "$100"
