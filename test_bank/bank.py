50dfdd308a69194ced846a943c4c83affa589b08
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-19 14:50:09 +0000

    Mon, Jun 19, 2023, 4:50 PM +02:00

--- test_bank/bank.py
+++ test_bank/bank.py
@@ -3,19 +3,19 @@ from cs50 import get_string
 
 greeting = input("Greeting: ").lower().lstrip()
 
-if greeting[0:5] == "hello":
-    print("$0", end="")
-elif greeting[0:1] == 'h':
-    print("$20", end="")
-else:
-    print("$100", end="")
+
 
 def main():
     ...
 
 
 def value(greeting):
-    ...
+    if greeting[0:5] == "hello":
+        print("$0", end="")
+    elif greeting[0:1] == 'h':
+        print("$20", end="")
+    else:
+        print("$100", end="")
 
 
 if __name__ == "__main__":
