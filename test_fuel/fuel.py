8b7655c87d03baa478c25f9a850dbab894bfa749
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-20 06:44:41 +0000

    Tue, Jun 20, 2023, 8:44 AM +02:00

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -35,10 +35,18 @@ def convert(fraction):
         fraction = input("Fraction: ")
     if y == 0:
         fraction = input("Fraction: ")
+    percentage = int((x / y) * 100)
+
+    return percentage
 
 
 def gauge(percentage):
-    ...
+    if percentage <= 1:
+        return ("E")
+    elif percentage >= 99:
+        return ("F")
+    else:
+        return (str(percentage) + "%")
 
 
 if __name__ == "__main__":
