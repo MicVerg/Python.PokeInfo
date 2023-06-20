c1ea11ded5c7b7cc3f779ec270d2835aa023e788
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-20 07:36:21 +0000

    Tue, Jun 20, 2023, 9:36 AM +02:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -16,4 +16,10 @@ def test_convert_80():
     assert convert("80 / 100") == 80
 
 def test_gauge_1():
-    assert gauge("1") == 'E'
+    assert gauge(int("1")) == 'E'
+
+def test_gauge_99():
+    assert gauge(int("99")) == 'F'
+
+def test_gauge_50():
+    assert gauge(int("50")) == '50%'
