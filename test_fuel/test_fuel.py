5c09e9cf26ac4e5d10ea6aab344c89307cb7e8d0
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-20 07:27:39 +0000

    Tue, Jun 20, 2023, 9:27 AM +02:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -5,4 +5,9 @@ def test_convert_string():
     with pytest.raises(ValueError):
         convert("cat")
 
-def test_convert_
+def test_convert_y0():
+    with pytest.raises(ZeroDivisionError):
+        convert("-1 / 0")
+
+def test_convert_40():
+    assert convert("40 / 100") == 40
