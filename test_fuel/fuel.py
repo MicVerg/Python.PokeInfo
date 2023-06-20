fa3ea44345b100623381a5cdd01932a0f29deda0
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-20 07:03:08 +0000

    Tue, Jun 20, 2023, 9:03 AM +02:00

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -1,27 +1,4 @@
 
-while True:
-    try:
-        fraction = input("Fraction: ")
-        split = user_input.split('/')
-        x = int(split[0])
-        y = int(split[1])
-        if x > y:
-            user_input = input("Fraction: ")
-        if y == 0:
-            user_input = input("Fraction: ")
-
-        percentage = int((x / y) * 100)
-        if percentage <= 1:
-            print("E")
-        elif percentage >= 99:
-            print("F")
-        else:
-            print(str(percentage) + "%")
-
-    except (ValueError, ZeroDivisionError):
-        pass
-    else: 
-        break
 
 def main():
     fraction = input("Fraction: ")
