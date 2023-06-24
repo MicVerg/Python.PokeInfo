1b7e5fbbe34b3185494a0f364b17275cbb44fbf5
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-24 08:30:15 +0000

    Sat, Jun 24, 2023, 10:30 AM +02:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -1,8 +1,8 @@
 import sys, csv
 
-if len(sys.argv) < 2:
+if len(sys.argv) < 3:
     sys.exit("Too few command-line arguments")
-elif len(sys.argv) > 2:
+elif len(sys.argv) > 3:
     sys.exit("Too many command-line arguments")
 else:
     try:
