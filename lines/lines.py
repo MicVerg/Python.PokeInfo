03024ad83780365efeaa786cd0b4b0dafbfd3f50
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-24 07:43:20 +0000

    Sat, Jun 24, 2023, 9:43 AM +02:00

---
+++ lines/lines.py
@@ -0,0 +1,21 @@
+import sys
+
+# if not 1 command argument, or file ends with .py, or file doesnt exist, sys.exit
+line_counter = 0
+
+if len(sys.argv) < 2:
+    sys.exit("Too few command-line arguments")
+elif len(sys.argv) > 2:
+    sys.exit("Too many command-line arguments")
+else:
+    if not sys.argv[1].endswith(".py"):
+        sys.exit("Not a Python file")
+    else:
+        with open(sys.argv[1], "r") as file:
+            try:
+                for line in file:
+                    if not line.startswith('#') or not line.startswith(' '):
+                        line_counter += 1
+                print(line_counter)
+            except (FileNotFoundError):
+                sys.exit("File does not exist")
