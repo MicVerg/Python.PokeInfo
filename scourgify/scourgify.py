8a27288d0a9d90969ea8fddad7812c21b080d5d5
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-24 08:28:53 +0000

    Sat, Jun 24, 2023, 10:28 AM +02:00

---
+++ scourgify/scourgify.py
@@ -0,0 +1,10 @@
+import sys, csv
+
+if len(sys.argv) < 2:
+    sys.exit("Too few command-line arguments")
+elif len(sys.argv) > 2:
+    sys.exit("Too many command-line arguments")
+else:
+    try:
+    except(FileNotFoundError):
+        sys.exit("Could not read " + sys.argv[1] + " invalid_file.csv")
