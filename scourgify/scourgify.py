f902da8788f5c0ef8d8a5a15072d43f2fb65ac02
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-25 07:10:30 +0000

    Sun, Jun 25, 2023, 9:10 AM +02:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -1,4 +1,8 @@
 import sys, csv
+first_names = []
+last_names = []
+house = []
+
 
 if len(sys.argv) < 3:
     sys.exit("Too few command-line arguments")
@@ -9,6 +13,6 @@ else:
         with open(sys.argv[1]) as file:
             reader = csv.DictReader(file)
             for row in reader:
-                
+
     except(FileNotFoundError):
         sys.exit("Could not read " + sys.argv[1] + " invalid_file.csv")
