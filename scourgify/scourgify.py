2c32392bcb3f0724582b062efe79ec5908943b88
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-25 07:46:22 +0000

    Sun, Jun 25, 2023, 9:46 AM +02:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -13,20 +13,22 @@ else:
         with open(sys.argv[1]) as file:
             reader = csv.DictReader(file)
             for row in reader:
-                first_names = row['name'].split(',')
+                name_parts = row['name'].split(',')
 
-                last_names.append(first_names[0])
-                first_names.append(first_names[1])
+                last_names.append(name_parts[0].strip())
+                first_names.append(name_parts[1].strip())
                 house.append(row['house'])
-        data_to_write = [first_names, last_names, house]
-        print(data_to_write)
-        with open('after.csv', 'w') as file:
+
+        with open('after.csv', 'w', newline='') as file:
             writer = csv.DictWriter(file, fieldnames=fieldnames)
-            for entry in first_names:
-                writer.writerow(data_to_write)
-
-
-
+            writer.writeheader()
+            
+            for i in range(len(first_names)):
+                writer.writerow({
+                    'first': first_names[i],
+                    'last': last_names[i],
+                    'house': house[i]
+                })
 
     except(FileNotFoundError):
         sys.exit("Could not read " + sys.argv[1] + " invalid_file.csv")
