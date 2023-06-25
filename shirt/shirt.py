78223e42e527ceb0b90f7ea969214c641c19d571
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-25 08:03:34 +0000

    Sun, Jun 25, 2023, 10:03 AM +02:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -6,5 +6,9 @@ if len(sys.argv) < 3:
 elif len(sys.argv) > 3:
     sys.exit("Too many command-line arguments")
 elif splitext(argv[1])[1] != '.jpg' or splitext(argv[1])[1] != '.jpeg' or splitext(argv[1])[1] != '.png':
+    sys.exit("Invalid input")
+elif splitext(argv[2])[1] != '.jpg' or splitext(argv[2])[1] != '.jpeg' or splitext(argv[2])[1] != '.png':
     sys.exit("Invalid output")
+elif splitext(argv[1])[1] != splitext(argv[2])[1]:
+    sys.exit("Input and output have different extensions")
 else:
