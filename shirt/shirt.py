7bc7a0acdb11c7e88bbaa1ce83fa49127db6311d
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-25 08:20:49 +0000

    Sun, Jun 25, 2023, 10:20 AM +02:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -1,19 +1,19 @@
 import sys, pip, os
 from os.path import splitext
 
+try:
+    if len(sys.argv) < 3:
+        sys.exit("Too few command-line arguments")
+    if len(sys.argv) > 3:
+        sys.exit("Too many command-line arguments")
+    if splitext(sys.argv[1])[1] != '.jpg' and splitext(sys.argv[1])[1] != '.jpeg' and splitext(sys.argv[1])[1] != '.png':
+        sys.exit("Invalid input")
+    if splitext(sys.argv[2])[1] != '.jpg' and splitext(sys.argv[2])[1] != '.jpeg' and splitext(sys.argv[2])[1] != '.png':
+        sys.exit("Invalid output")
+    if splitext(sys.argv[1])[1] != splitext(sys.argv[2])[1]:
+        sys.exit("Input and output have different extensions")
+    else:
+        shirt = Image.open("shirt.png")
 
-if len(sys.argv) < 3:
-    sys.exit("Too few command-line arguments")
-if len(sys.argv) > 3:
-    sys.exit("Too many command-line arguments")
-if splitext(sys.argv[1])[1] != '.jpg' and splitext(sys.argv[1])[1] != '.jpeg' and splitext(sys.argv[1])[1] != '.png':
-    sys.exit("Invalid input")
-if splitext(sys.argv[2])[1] != '.jpg' and splitext(sys.argv[2])[1] != '.jpeg' and splitext(sys.argv[2])[1] != '.png':
-    sys.exit("Invalid output")
-if splitext(sys.argv[1])[1] != splitext(sys.argv[2])[1]:
-    sys.exit("Input and output have different extensions")
-else:
-    try:
-        
-    except(FileNotFoundError):
-        sys.exit("Input does not exist)
+except(FileNotFoundError):
+    sys.exit("Input does not exist")
