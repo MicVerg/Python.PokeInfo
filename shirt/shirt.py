1b6ad8afc18caf47d887212aa6e7ea1069ba8a4b
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-25 08:00:25 +0000

    Sun, Jun 25, 2023, 10:00 AM +02:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -5,5 +5,6 @@ if len(sys.argv) < 3:
     sys.exit("Too few command-line arguments")
 elif len(sys.argv) > 3:
     sys.exit("Too many command-line arguments")
-elif splitext(argv[1]).ext
+elif splitext(argv[1])[1] != '.jpg' or splitext(argv[1])[1] != '.jpeg' or splitext(argv[1])[1] != '.png':
+    sys.exit("Invalid output")
 else:
