41589894e8973b96dd3930553abc0121636f8ee1
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-28 14:15:30 +0000

    Wed, Jun 28, 2023, 4:15 PM +02:00

--- response/response.py
+++ response/response.py
@@ -2,3 +2,17 @@ import re
 import sys
 import validators
 
+
+def main():
+    print(validate(input("What's your email address? ")))
+
+
+def validate(s):
+    if validators.email(s) == False:
+        return "Invalid"
+    else:
+        return "Valid"
+
+
+if __name__ == "__main__":
+    main()
