92152fb0e59d89850f952af3723d566c55fd1e38
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-28 12:53:41 +0000

    Wed, Jun 28, 2023, 2:53 PM +02:00

--- working/test_working.py
+++ working/test_working.py
@@ -1,2 +1,16 @@
 from working import new_format, convert
 
+def test1():
+    assert convert("09:00 AM to 5:00 PM") == "09:00 to 17:00"
+
+
+def test2():
+    assert convert("9 AM to 5 PM") == "09:00 to 17:00"
+
+
+def test3():
+    assert convert("9 AM to 5:30 PM") == "09:00 to 17:30"
+
+
+def test4():
+    assert convert("10:7 AM - 5:1 PM") == ValueError
