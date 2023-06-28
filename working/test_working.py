30cf7e44537d6280f3af6cb8da556c3fef5cba17
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-28 13:21:38 +0000

    Wed, Jun 28, 2023, 3:21 PM +02:00

--- working/test_working.py
+++ working/test_working.py
@@ -1,8 +1,8 @@
 from working import new_format, convert
 import pytest
 
-# def test1():
-  #  assert convert("09:00 AM to 5:00 PM") == "09:00 to 17:00"
+def test1():
+    assert convert("09:00 AM to 5:00 PM") == "09:00 to 17:00"
 
 
 def test2():
