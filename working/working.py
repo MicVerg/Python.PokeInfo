2965214b8d1e12dcc5c92b91bb07086663f08d9c
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-06-28 12:14:24 +0000

    Wed, Jun 28, 2023, 2:14 PM +02:00

--- working/working.py
+++ working/working.py
@@ -19,6 +19,17 @@ def convert(s):
 
 def new_format(hour, minute, am_pm):
     if am_pm == 'PM':
-        
+        if int(hour) == 12:
+            new_hour = 12
+        else:
+            new_hour = int(hour) + 12
+    else:
+        if int(hour) == 12:
+            new_hour = 0
+        else:
+            new_hour = int(hour)
+
+
+
 if __name__ == "__main__":
     main()
