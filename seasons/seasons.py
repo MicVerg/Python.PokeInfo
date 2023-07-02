b69547cd39563e87338588d0e099a96c31af219c
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-07-02 16:26:36 +0000

    Sun, Jul 2, 2023, 6:26 PM +02:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -8,8 +8,12 @@ def main():
     try:
         user_input = input("Date of birth: ")
         user_input_date = datetime.strptime(user_input, "%Y-%m-%d")
-        current_date = datetime.date()
-        
+        current_date = datetime.now()
+
+        difference_date = current_date - user_input_date
+        print(difference_date)
+    except 
+
 
 
 
