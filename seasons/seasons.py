c76062a6285c7298ca4b21c7f76820734d6258fa
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-07-02 17:02:51 +0000

    Sun, Jul 2, 2023, 7:02 PM +02:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -12,7 +12,8 @@ def main():
 
         difference_date = current_date - user_input_date
         difference_date_minutes = difference_date.days * 24 * 60
-        difference_in_words = p.number_to_words(difference_date_minutes)
+        difference_in_words = p.number_to_words(difference_date_minutes, andword="")
+        
         print(difference_in_words.capitalize() + " minutes")
     except:
         sys.exit("Invalid date")
