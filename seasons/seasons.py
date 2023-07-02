610aebecdc6ce6b41db93391d4252ea7bdfd5725
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-07-02 16:48:26 +0000

    Sun, Jul 2, 2023, 6:48 PM +02:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -11,7 +11,7 @@ def main():
         current_date = datetime.now()
 
         difference_date = current_date - user_input_date
-        difference_date_minutes = round(difference_date.total_seconds() / 60)
+        difference_date_minutes = (difference_date.total_seconds() / 60)
         difference_in_words = p.number_to_words(difference_date_minutes)
         print(difference_in_words.capitalize() + " minutes")
     except:
