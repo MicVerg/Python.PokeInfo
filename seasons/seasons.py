cf42a02991eacf57e35b9e4ecb12b713e128a58f
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-07-02 16:49:44 +0000

    Sun, Jul 2, 2023, 6:49 PM +02:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -8,10 +8,10 @@ def main():
     try:
         user_input = input("Date of birth: ")
         user_input_date = datetime.strptime(user_input, "%Y-%m-%d")
-        current_date = datetime.now()
+        current_date = date.today()
 
         difference_date = current_date - user_input_date
-        difference_date_minutes = (difference_date.total_seconds() / 60)
+        difference_date_minutes = round(difference_date.total_seconds() / 60)
         difference_in_words = p.number_to_words(difference_date_minutes)
         print(difference_in_words.capitalize() + " minutes")
     except:
