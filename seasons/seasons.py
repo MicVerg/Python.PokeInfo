b086815a9cfa7cc732f4a12b7df99cd688c413f1
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-07-02 16:55:15 +0000

    Sun, Jul 2, 2023, 6:55 PM +02:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -8,7 +8,7 @@ def main():
     try:
         user_input = input("Date of birth: ")
         user_input_date = datetime.strptime(user_input, "%Y-%m-%d")
-        current_date = datetime.today()
+        current_date = date.today()
 
         difference_date = current_date - user_input_date
         difference_date_minutes = round(difference_date.total_seconds() / 60)
