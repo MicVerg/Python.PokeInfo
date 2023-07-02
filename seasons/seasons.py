52eb7b3cf0c366233d97929fdf5e472cb319538d
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-07-02 17:06:25 +0000

    Sun, Jul 2, 2023, 7:06 PM +02:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -8,12 +8,12 @@ def main():
     try:
         user_input = input("Date of birth: ")
         user_input_date = datetime.strptime(user_input, "%Y-%m-%d")
-        current_date = datetime.today()
+        current_date = date.today()
 
         difference_date = current_date - user_input_date
         difference_date_minutes = difference_date.days * 24 * 60
         difference_in_words = p.number_to_words(difference_date_minutes, andword="")
-        
+
         print(difference_in_words.capitalize() + " minutes")
     except:
         sys.exit("Invalid date")
