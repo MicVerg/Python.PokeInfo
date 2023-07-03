ebffc41e648dada530b1298ca48294e23eab29fb
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-07-03 13:41:41 +0000

    Mon, Jul 3, 2023, 3:41 PM +02:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -1,7 +1,9 @@
 from fpdf import FPDF
 
+user_input = input("Name: ")
+
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 pdf.add_page()
 pdf.set_font("helvetica", "B", 16)
-pdf.cell(40, 10, "Hello World!")
+pdf.cell(40, 10, user_input)
 pdf.output("shirtificate.pdf")
