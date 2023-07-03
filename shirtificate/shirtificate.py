8e00e8d31f4e7b8351500e0c374fc049c8a22589
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-07-03 13:48:07 +0000

    Mon, Jul 3, 2023, 3:48 PM +02:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -2,10 +2,14 @@ from fpdf import FPDF
 
 user_input = input("Name: ")
 
-def set_text_color(self, 255, 255, 255):
+# def set_text_color(self, 255, 255, 255):
 
-    pdf = FPDF(orientation="P", unit="mm", format="A4")
-    pdf.add_page()
-    pdf.set_font("helvetica", "B", 16)
-    pdf.cell(40, 10, user_input)
-    pdf.output("shirtificate.pdf")
+class PDF(FPDF):
+    def header(self):
+        self.image("../shirtificate.png", 10, 8, 33)
+        self.set_font("helvetica", "B", 16)
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.add_page()
+
+pdf.cell(40, 10, user_input)
+pdf.output("shirtificate.pdf")
