c0dbffaca64dd2755a6609fb3b04fe5e1e7e8504
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-07-03 14:35:32 +0000

    Mon, Jul 3, 2023, 4:35 PM +02:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -14,15 +14,15 @@ class PDF(FPDF):
         self.image("./shirtificate.png", x = 30, y = 50, w = 150, h = 150)
         self.set_font("helvetica", "B", 20)
         self.set_text_color(255, 255, 255)
-        
+
 
 def main():
     user_input = input("Name: ")
     pdf = PDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
-    pdf.output("shirtificate.pdf")
     shirt_text = f"{user_input} took CS50"
     pdf.body(shirt_text)
+    pdf.output("shirtificate.pdf")
 
 
 if __name__ == "__main__":
