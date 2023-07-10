b7827fbfa29f26c4cc280a04e4c1bfd16b424b64
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-07-10 15:42:44 +0000

    Mon, Jul 10, 2023, 5:42 PM +02:00

--- projectp/project.py
+++ projectp/project.py
@@ -27,7 +27,11 @@ class App(customtkinter.CTk):
             switch = customtkinter.CTkSwitch(master=self.scrollable_frame, text=f"CTkSwitch {i}")
             switch.grid(row=i, column=0, padx=10, pady=(0, 20))
             self.scrollable_frame_switches.append(switch)
-
+        
+        self.slider_progressbar_frame = customtkinter.CTkFrame(self, fg_color="transparent")
+        self.slider_progressbar_frame.grid(row=1, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
+        self.slider_progressbar_frame.grid_columnconfigure(0, weight=1)
+        self.slider_progressbar_frame.grid_rowconfigure(4, weight=1)
         self.seg_button_1 = customtkinter.CTkSegmentedButton(self.slider_progressbar_frame)
         self.seg_button_1.grid(row=0, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
 
