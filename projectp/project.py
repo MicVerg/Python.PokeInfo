3358b226a292af05b01fef4510e0e62e973ae8a7
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-07-10 13:15:21 +0000

    Mon, Jul 10, 2023, 3:15 PM +02:00

--- projectp/project.py
+++ projectp/project.py
@@ -3,26 +3,31 @@ import customtkinter
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
 
-root = customtkinter.Ctk()
+root = customtkinter.CTk()
 root.geometry("500x350")
 
 
 
-def_login():
+def login():
     print("Test")
 
 
-frame = customtkinter.CtkFrame(master=root)
+frame = customtkinter.CTkFrame(master=root)
 frame.pack(pady=20, padx=60, fill="both", expand=True)
 
-label = customtkinter.CtkLabel(master=frame, text="Login system", text_font=("Arial, 24))
+label = customtkinter.CTkLabel(master=frame, text="Login system")
 label.pack(pady=12, padx=10)
 
-entry1 = customtkinter.CtkEntry(master=frame, placeholder_text="Username")
+entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Username")
 entry1.pack(pady=12, padx=10)
 
-entry1 = customtkinter.CtkEntry(master=frame, placeholder_text="Password", show="*")
+entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Password", show="*")
 entry1.pack(pady=12, padx=10)
 
-button = customtkinter.CtkButton(master=frame, text="Login", command=login)
-button.pack(pady)
+button = customtkinter.CTkButton(master=frame, text="Login", command=login)
+button.pack(pady=12, padx=10)
+
+checkbox = customtkinter.CTkCheckBox(master=frame, text="Remember me")
+checkbox.pack(pady=12, padx=10)
+
+root.mainloop()
