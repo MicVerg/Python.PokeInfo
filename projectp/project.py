a68c592ef0b54d780defb9b92e5439f0eebd4de7
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-07-11 09:12:26 +0000

    Tue, Jul 11, 2023, 11:12 AM +02:00

--- projectp/project.py
+++ projectp/project.py
@@ -24,7 +24,7 @@ class App(customtkinter.CTk):
         self.scrollable_frame.grid_columnconfigure(0, weight=1)
         self.scrollable_frame_switches = []
 
-        self.slider_progressbar_frame = customtkinter.CTkFrame(self, fg_color="transparent")
+        self.slider_progressbar_frame = customtkinter.CTkFrame(self, fg_color="transparent", label_font=("Montserrat", 16))
         self.slider_progressbar_frame.grid(row=1, column=0, columnspan=3, padx=(20, 0), pady=(0, 20), sticky="nsew")
         self.slider_progressbar_frame.grid_columnconfigure(0, weight=1)
         self.slider_progressbar_frame.grid_rowconfigure(4, weight=1)
