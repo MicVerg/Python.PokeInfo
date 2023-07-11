dac188a5370693fe1410e1aa7facb1570495c401
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-07-11 11:39:19 +0000

    Tue, Jul 11, 2023, 1:39 PM +02:00

--- projectp/project.py
+++ projectp/project.py
@@ -85,8 +85,8 @@ class App(customtkinter.CTk):
         self.scrollable_frame_switches.remove(switch_frame.winfo_children()[0])
 
 
-    def open_list_item():
-        
+    def open_list_window():
+
 
 
 
