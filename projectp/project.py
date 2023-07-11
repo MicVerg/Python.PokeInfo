7bad54c1962676a900a214c77c540855c6d9fe3f
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-07-11 08:40:50 +0000

    Tue, Jul 11, 2023, 10:40 AM +02:00

--- projectp/project.py
+++ projectp/project.py
@@ -43,19 +43,19 @@ class App(customtkinter.CTk):
     def add_list_window(self):
         add_window = tkinter.Toplevel(self)
         add_window.title("Add a list")
-        add_window.geometry("350x200")
+        
 
         add_textbox = customtkinter.CTkTextbox(add_window)
-        add_textbox.grid(row=0, column=0, padx=10, pady=10)
+        add_textbox.pack(padx=10, pady=10)
 
         button_frame = tkinter.Frame(add_window)
-        button_frame.grid(row=1, column=0, padx=10, pady=5)
+        button_frame.pack(pady=5)
 
         confirm_button = tkinter.Button(button_frame, text="Confirm", command=lambda: self.add_list(add_textbox))
-        confirm_button.grid(row=0, column=0, padx=5)
+        confirm_button.pack(side="left", padx=5)
 
         cancel_button = tkinter.Button(button_frame, text="Cancel", command=add_window.destroy)
-        cancel_button.grid(row=0, column=1, padx=5)
+        cancel_button.pack(side="left", padx=5)
 
     def add_list(self, add_textbox):
         add_text = add_textbox.get("1.0", tkinter.END)
