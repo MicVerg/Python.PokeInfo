dce1298dba77fa4d171edbd37ecb3d5c48fea1f4
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-07-11 09:15:54 +0000

    Tue, Jul 11, 2023, 11:15 AM +02:00

--- projectp/project.py
+++ projectp/project.py
@@ -24,7 +24,7 @@ class App(customtkinter.CTk):
         self.scrollable_frame.grid_columnconfigure(0, weight=1)
         self.scrollable_frame_switches = []
 
-        self.slider_progressbar_frame = customtkinter.CTkFrame(self, fg_color="transparent", label_font=("Montserrat", 16))
+        self.slider_progressbar_frame = customtkinter.CTkFrame(self, fg_color="transparent")
         self.slider_progressbar_frame.grid(row=1, column=0, columnspan=3, padx=(20, 0), pady=(0, 20), sticky="nsew")
         self.slider_progressbar_frame.grid_columnconfigure(0, weight=1)
         self.slider_progressbar_frame.grid_rowconfigure(4, weight=1)
@@ -62,6 +62,7 @@ class App(customtkinter.CTk):
             switch = customtkinter.CTkSwitch(master=self.scrollable_frame, text=add_text)
             switch.grid(row=len(self.scrollable_frame_switches), column=0, padx=10, pady=(0, 20))
             self.scrollable_frame_switches.append(switch)
+        add_textbox.master.destroy()
 
 
 def remove_list():
