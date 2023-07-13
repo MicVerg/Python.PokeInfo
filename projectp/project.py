e581ce0c29c58dd8393ebdb268eb7a8f05c26003
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-07-13 06:41:34 +0000

    Thu, Jul 13, 2023, 8:41 AM +02:00

--- projectp/project.py
+++ projectp/project.py
@@ -1,7 +1,6 @@
 import tkinter
 from PIL import Image
 import customtkinter
-from functools import partial
 
 customtkinter.set_appearance_mode("Dark")
 customtkinter.set_default_color_theme("dark-blue")
@@ -17,13 +16,19 @@ vegetables_image = customtkinter.CTkImage(light_image=Image.open("./img/icons8-v
 grains_image = customtkinter.CTkImage(light_image=Image.open("./img/icons8-grains-64.png"), dark_image=Image.open("./img/icons8-grains-64.png"))
 dairy_image = customtkinter.CTkImage(light_image=Image.open("./img/icons8-cheese-48.png"), dark_image=Image.open("./img/icons8-cheese-48.png"))
 spices_image = customtkinter.CTkImage(light_image=Image.open("./img/icons8-spices-48.png"), dark_image=Image.open("./img/icons8-spices-48.png"))
-remove_image = customtkinter.CTkImage(light_image=Image.open("./img/icons8-alcohol-30.png"), dark_image=Image.open("./img/icons8-alcohol-30.png"))
+alcohol_image = customtkinter.CTkImage(light_image=Image.open("./img/icons8-alcohol-30.png"), dark_image=Image.open("./img/icons8-alcohol-30.png"))
+remove_image = customtkinter.CTkImage(light_image=Image.open("./img/icons8-remove-48.png"), dark_image=Image.open("./img/icons8-remove-48.png"))
+questionmark_image = customtkinter.CTkImage(light_image=Image.open("./img/icons8-question-mark-48.png"), dark_image=Image.open("./img/icons8-question-mark-48.png"))
 
 
 
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
+
+        self.list_of_items = []
+        self.list_of_amounts = []
+        self.lists_data = {}
 
         # configure window
         self.title("Grocery lists")
@@ -55,7 +60,8 @@ class App(customtkinter.CTk):
         add_window.transient(self)
         add_window.geometry("300x300")
 
-        add_textbox = customtkinter.CTkTextbox(add_window, font=("Montserrat", 22), border_color="light_color")
+        add_textbox = customtkinter.CTkTextbox(add_window, font=("Montserrat", 22))
+        add_textbox.configure(height=8)
         add_textbox.pack(padx=10, pady=10)
 
         button_frame = customtkinter.CTkFrame(add_window)
@@ -69,7 +75,7 @@ class App(customtkinter.CTk):
 
 
     def add_list(self, add_textbox):
-        add_text = add_textbox.get("1.0", tkinter.END).strip()  # Strip any leading/trailing whitespace
+        add_text = add_textbox.get("1.0", tkinter.END).strip()
         if add_text:
             switch_frame = customtkinter.CTkFrame(self.scrollable_frame)
             switch_frame.grid(row=len(self.scrollable_frame_switches), column=0, padx=10, pady=(0, 20))
@@ -83,15 +89,14 @@ class App(customtkinter.CTk):
             open_button = customtkinter.CTkButton(button_frame, text="Open", command=lambda: self.open_list_window(add_text))
             open_button.pack(side="left")
 
-            # remove button
-            remove_image = customtkinter.CTkImage(light_image=Image.open("./img/icons8-remove-48.png"), dark_image=Image.open("./img/icons8-remove-48.png"))
             remove_button = customtkinter.CTkButton(button_frame, image=remove_image, text="", command=lambda: self.remove_list_item(switch_frame))
             remove_button.pack(side="left", padx=5)
 
             switch_frame.grid_columnconfigure(0, weight=1)
             self.scrollable_frame_switches.append(switch)
+            self.lists_data[add_text] = []  # Initialize an empty list for the added list
 
-        add_textbox.master.destroy()  # Close the popup window after adding the item
+        add_textbox.master.destroy()
 
 
     def remove_list_item(self, switch_frame):
@@ -100,82 +105,136 @@ class App(customtkinter.CTk):
 
 
     def open_list_window(self, list_name):
-        open_window = customtkinter.CTkToplevel(self)
-        open_window.title(list_name)
-        open_window.transient(self)
-        open_window.geometry("375x667")
+        self.open_window = customtkinter.CTkToplevel(self)
+        self.open_window.title(list_name)
+        self.open_window.transient(self)
+        self.open_window.geometry("375x600")
 
-        # Create a frame for the three columns
-        columns_frame = customtkinter.CTkFrame(open_window)
-        columns_frame.pack(fill="both", expand=True, padx=10, pady=10)
+        # Retrieve the list's data from lists_data dictionary
+        list_data = self.lists_data[list_name]
 
-        # Column 1: Image
-        image_frame = customtkinter.CTkFrame(columns_frame)
-        image_frame.pack(side="left", padx=10)
+        for item, amount in list_data:
+            item_frame = customtkinter.CTkFrame(self.open_window)
+            item_frame.pack()
 
-        # Add your image widget or label here
+            checkbox_var = tkinter.IntVar(value=0)
+            checkbox = customtkinter.CTkCheckBox(item_frame, text="", variable=checkbox_var)
+            checkbox.grid(row=0, column=0, padx=5, sticky="w")
 
-        # Column 2: Description
-        description_frame = customtkinter.CTkFrame(columns_frame)
-        description_frame.pack(side="left", padx=10)
+            description_label = customtkinter.CTkLabel(item_frame, text=item, font=("Montserrat", 14))
+            description_label.grid(row=0, column=1, padx=(0, 10), sticky="w")
 
-        # Add your description widget or label here
+            if item in fruits_list:
+                correct_image = fruits_image
+            elif item in vegetables_list:
+                correct_image = vegetables_image
+            elif item in grains_list:
+                correct_image = grains_image
+            elif item in dairy_list:
+                correct_image = dairy_image
+            elif item in spices_list:
+                correct_image = spices_image
+            elif item in alcohol_list:
+                correct_image = alcohol_image
+            else:
+                # Handle the case when description doesn't match any category
+                correct_image = questionmark_image
 
-        # Column 3: Checkbox
-        checkbox_frame = customtkinter.CTkFrame(columns_frame)
-        checkbox_frame.pack(side="left", padx=10)
+            item_image = customtkinter.CTkLabel(item_frame, text="", image=correct_image)
+            item_image.grid(row=0, column=2, padx=(0, 10))
 
-        checkbox = customtkinter.CTkCheckBox(checkbox_frame, text="Select")
-        checkbox.pack(side="left")
+            delete_button = customtkinter.CTkButton(item_frame, image=remove_image, text="", command=lambda: self.remove_item(item_frame, list_name, item))
+            delete_button.grid(row=0, column=3, padx=5)
+
+
+        # Add the "Add" button to the open window
+        add_button = customtkinter.CTkButton(self.open_window, text="Add", command=self.add_item_window)
+        add_button.pack(side="bottom", pady=10)
+
 
 
     def add_item_window(self):
         add_item_window = customtkinter.CTkToplevel(self)
         add_item_window.title("Add your items")
         add_item_window.transient(self)
-        add_item_window.geometry("375x667")
+        add_item_window.geometry("375x400")
 
-        add_item_textbox = customtkinter.CTkTextbox(add_item_window, font=("Montserrat", 22))
-        add_item_textbox.pack(padx=10, pady=10)
+        description_label = customtkinter.CTkLabel(add_item_window, text="Description:")
+        description_label.grid(row=0, column=0, padx=10, pady=5)
 
-        button_item_frame = customtkinter.CTkFrame(add_item_window)
-        button_item_frame.pack(pady=5)
+        description_textbox = customtkinter.CTkTextbox(add_item_window, font=("Montserrat", 20))
+        description_textbox.configure(height=8)
+        description_textbox.grid(row=1, column=0, padx=10, pady=5)
 
-        confirm_item_button = customtkinter.CTkButton(button_item_frame, text="Confirm", command=lambda: self.add_item(add_item_textbox))
-        confirm_item_button.pack(side="left", padx=5)
+        amount_label = customtkinter.CTkLabel(add_item_window, text="Amount/Unit:")
+        amount_label.grid(row=2, column=0, padx=10, pady=5)
 
-        cancel_item_button = customtkinter.CTkButton(button_item_frame, text="Cancel", command=add_item_window.destroy)
-        cancel_item_button.pack(side="left", padx=5)
+        amount_textbox = customtkinter.CTkTextbox(add_item_window, font=("Montserrat", 20))
+        amount_textbox.configure(height=8)
+        amount_textbox.grid(row=3, column=0, padx=10, pady=5)
 
+        button_frame = customtkinter.CTkFrame(add_item_window)
+        button_frame.grid(row=4, column=0, padx=10, pady=5)
 
-    def add_item(self, add_item_textbox):
-        add_item_text = add_item_textbox.get("1.0", tkinter.END).strip()  # Strip any leading/trailing whitespace
-        if add_item_text:
-            switch_item_frame = customtkinter.CTkFrame(self.scrollable_frame)
-            switch_item_frame.grid(row=len(self.scrollable_frame_switches), column=0, padx=10, pady=(0, 20))
+        confirm_button = customtkinter.CTkButton(button_frame, text="Add", command=lambda: self.add_item(description_textbox, amount_textbox))
+        confirm_button.grid(row=0, column=0, padx=5)
 
-            item_checkbox = customtkinter.CTkCheckBox(master=switch_frame, text=add_item_text, font=("Montserrat", 20))
-            item_checkbox.grid(row=0, column=0)
+        cancel_button = customtkinter.CTkButton(button_frame, text="Cancel", command=add_item_window.destroy)
+        cancel_button.grid(row=0, column=1, padx=5)
 
-            button_frame = customtkinter.CTkFrame(switch_frame)
-            button_frame.grid(row=1, column=0, padx=5, pady=(10, 0))
-
-            open_button = customtkinter.CTkButton(button_frame, text="Open", command=lambda: self.open_list_window(add_text))
-            open_button.pack(side="left")
-
-            # remove button
-            remove_image = customtkinter.CTkImage(light_image=Image.open("./img/icons8-remove-48.png"), dark_image=Image.open("./img/icons8-remove-48.png"))
-            remove_button = customtkinter.CTkButton(button_frame, image=remove_image, text="", command=lambda: self.remove_list_item(switch_frame))
-            remove_button.pack(side="left", padx=5)
-
-            switch_item_frame.grid_columnconfigure(0, weight=1)
-            self.scrollable_frame_switches.append(item_checkbox)
-
-        add_item_textbox.master.destroy()
+        add_item_window.grab_set()
 
 
-def remove_item():
-    print("Remove item")
+    def add_item(self, description_textbox, amount_textbox):
+        description = description_textbox.get("1.0", tkinter.END).strip()
+        amount = amount_textbox.get("1.0", tkinter.END).strip()
+
+        if description and amount:
+            item_frame = customtkinter.CTkFrame(self.open_window)
+            item_frame.pack()
+
+            checkbox_var = tkinter.IntVar(value=0)
+            checkbox = customtkinter.CTkCheckBox(item_frame, text="", variable=checkbox_var)
+            checkbox.grid(row=0, column=0, padx=5, sticky="w")
+
+            description_label = customtkinter.CTkLabel(item_frame, text=f"{description} - {amount}", font=("Montserrat", 14))
+            description_label.grid(row=0, column=1, padx=(0, 10), sticky="w")
+
+            if description in fruits_list:
+                correct_image = fruits_image
+            elif description in vegetables_list:
+                correct_image = vegetables_image
+            elif description in grains_list:
+                correct_image = grains_image
+            elif description in dairy_list:
+                correct_image = dairy_image
+            elif description in spices_list:
+                correct_image = spices_image
+            elif description in alcohol_list:
+                correct_image = alcohol_image
+            else:
+                # Handle the case when description doesn't match any category
+                correct_image = questionmark_image
+
+            item_image = customtkinter.CTkLabel(item_frame, text="", image=correct_image)
+            item_image.grid(row=0, column=2, padx=(0, 10))
+
+            delete_button = customtkinter.CTkButton(item_frame, image=remove_image, text="", command=lambda: self.remove_item(item_frame, list_name, description))
+            delete_button.grid(row=0, column=3, padx=5)
+
+            # Update the corresponding list's data in the lists_data dictionary
+            list_name = self.open_window.title()
+            self.lists_data[list_name].append((description, amount))
+
+            # Clear the content of the textboxes
+            description_textbox.delete("1.0", tkinter.END)
+            amount_textbox.delete("1.0", tkinter.END)
+
+
+    def remove_item(self, item_frame, list_name, item):
+        item_frame.destroy()
+        self.lists_data[list_name] = [(desc, amount) for desc, amount in self.lists_data[list_name] if desc != item]
+
 
 
 def check_item():
