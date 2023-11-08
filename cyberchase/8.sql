42d965024ec0af714171946997c1ec27144cb8b4
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-11-08 16:58:05 +0000

    Wed, Nov 8, 2023, 5:58 PM +01:00

--- cyberchase/8.sql
+++ cyberchase/8.sql
@@ -1,2 +1,2 @@
-SELECT COUNT("title") FROM "episodes"
-WHERE "air_date" >= 2018-01-01 AND "air_date" <= 2023-12-31;
+SELECT "title" FROM "episodes"
+WHERE "air_date" BETWEEN 2018-01-01 AND 2023-12-31;
