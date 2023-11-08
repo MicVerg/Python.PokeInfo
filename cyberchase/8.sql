8de19f8951327ee8bc8f25481404094b7518bb96
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-11-08 17:07:23 +0000

    Wed, Nov 8, 2023, 6:07 PM +01:00

--- cyberchase/8.sql
+++ cyberchase/8.sql
@@ -1,2 +1,2 @@
-SELECT "title" FROM "episodes"
+SELECT COUNT("title") FROM "episodes"
 WHERE "air_date" >= '2018-01-01' AND "air_date" <= '2023-12-31';
--- cyberchase/9.sql
+++ cyberchase/9.sql
@@ -1,2 +1,2 @@
-SELECT COUNT("title")
-WHERE "air_date" >= 2002-01-01 <= 2007-12-31;
+SELECT COUNT("title") FROM "episodes"
+WHERE "air_date" >= '2002-01-01' AND "air_date" <= '2007-12-31';
