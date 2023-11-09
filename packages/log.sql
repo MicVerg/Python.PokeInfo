fd61423a0d8828fd956e7292383940926baf73ca
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-11-09 21:11:34 +0000

    Thu, Nov 9, 2023, 10:11 PM +01:00

--- packages/log.sql
+++ packages/log.sql
@@ -1,6 +1,16 @@
 
 -- *** The Lost Letter ***
+SELECT * FROM "packages"
+WHERE "from_address_id" = '900 Somerville Avenue';
 
+SELECT * FROM "packages"
+   ...> LIMIT 10;
+
+SELECT * FROM "addresses"
+LIMIT 10;
+
+SELECT * FROM "packages"
+WHERE "from_address_id" = (SELECT "id" FROM "addresses" WHERE "address" = '900 Somerville Avenue');
 -- *** The Devious Delivery ***
 
 -- *** The Forgotten Gift ***
