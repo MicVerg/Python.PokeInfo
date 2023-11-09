232923dae18c94a46c7b619e8b367bb63b98ccd6
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-11-09 21:17:18 +0000

    Thu, Nov 9, 2023, 10:17 PM +01:00

--- packages/log.sql
+++ packages/log.sql
@@ -11,6 +11,9 @@ LIMIT 10;
 
 SELECT * FROM "packages"
 WHERE "from_address_id" = (SELECT "id" FROM "addresses" WHERE "address" = '900 Somerville Avenue') AND "to_address_id" = (SELECT "id" FROM "addresses" WHERE "address" = '%Finnigan Street%');
+
+SELECT * FROM "packages"
+WHERE "contents" = '%Congratulatory%';
 -- *** The Devious Delivery ***
 
 -- *** The Forgotten Gift ***
