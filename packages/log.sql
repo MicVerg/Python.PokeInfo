e7fb8b73d9ec80190bea626ff101e0a3cd18b6fc
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-11-09 21:32:37 +0000

    Thu, Nov 9, 2023, 10:32 PM +01:00

--- packages/log.sql
+++ packages/log.sql
@@ -30,5 +30,13 @@ WHERE "package_id" = 5098;
 
 SELECT * FROM "addresses"
 WHERE "id" = 348;
--- *** The Forgotten Gift ***
 
+-- *** The Forgotten Gift ***
+SELECT * FROM "packages"
+WHERE "from_address_id" = (SELECT "id" FROM "addresses" WHERE "address" = '109 Tileston Street');
+
+SELECT * FROM "scans"
+WHERE "package_id" = 9523;
+
+SELECT * FROM "drivers"
+WHERE "id" = 17;
