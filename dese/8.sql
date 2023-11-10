59105ae891c357ca6af16240b33a5255d3884140
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-11-10 11:36:26 +0000

    Fri, Nov 10, 2023, 12:36 PM +01:00

--- dese/8.sql
+++ dese/8.sql
@@ -1,2 +1,11 @@
-SELECT "district_id", "pupils" FROM "expenditures"
+SELECT "expenditures"."district_id", "expenditures"."pupils"
+FROM "expenditures"
+JOIN schools
+ON
 
+
+
+SELECT Students.name, Courses.course_name
+FROM Students
+JOIN Courses
+ON Students.id = Courses.student_id;
