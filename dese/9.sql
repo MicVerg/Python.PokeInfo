661159be37b7eafda95e066f943baed4d07dfcf9
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-11-10 11:47:26 +0000

    Fri, Nov 10, 2023, 12:47 PM +01:00

--- dese/9.sql
+++ dese/9.sql
@@ -1,4 +1,5 @@
 SELECT "districts"."name", MIN("expenditures"."pupils")
 FROM "expenditures"
 JOIN "districts"
-ON "expenditures"."id" = "districts"."id";
+ON "expenditures"."id" = "districts"."id"
+WHERE "expenditures"."pupils" = 64;
