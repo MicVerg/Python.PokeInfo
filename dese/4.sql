416fdfb9d2ec88ff6269e2e80db5e232f2c775cf
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-11-10 10:13:40 +0000

    Fri, Nov 10, 2023, 11:13 AM +01:00

--- dese/4.sql
+++ dese/4.sql
@@ -1,2 +1,6 @@
-SELECT "city", COUNT(id) AS "Total_amount_of_schools"  FROM "schools"
-WHERE "type" = 'Public School';
+SELECT "city", COUNT(id) AS "Total_amount_of_schools"
+FROM "schools"
+WHERE "type" = 'Public School'
+GROUP BY "city"
+ORDER BY "Total_amount_of_schools" DESC
+LIMIT 10;
