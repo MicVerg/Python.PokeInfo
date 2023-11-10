c1fae1ee63f9a461cb5393c98dda76aa2d46acb2
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-11-10 10:48:29 +0000

    Fri, Nov 10, 2023, 11:48 AM +01:00

--- dese/5.sql
+++ dese/5.sql
@@ -1,5 +1,6 @@
 SELECT "city", COUNT(id) AS "Total_amount_of_schools"
 FROM "schools"
 WHERE "type" = 'Public School'
-GROUP BY "city" < 3
+GROUP BY "city"
+HAVING "Total_amount_of_schools" <3
 ORDER BY "Total_amount_of_schools" DESC, "city" ASC;
