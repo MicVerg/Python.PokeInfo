a40845438538d480b0668016c9ffe71a28d6888f
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-11-12 14:02:55 +0000

    Sun, Nov 12, 2023, 3:02 PM +01:00

--- dese/12.sql
+++ dese/12.sql
@@ -5,6 +5,3 @@ JOIN "staff_evaluations" ON "districts"."id" = "staff_evaluations"."district_id"
 WHERE "expenditures"."per_pupil_expenditure" > (SELECT AVG("expenditures"."per_pupil_expenditure") FROM "expenditures")
 AND "staff_evaluations"."exemplary" > (SELECT AVG("staff_evaluations"."exemplary") FROM "staff_evaluations")
 ORDER BY "staff_evaluations"."exemplary" DESC, "expenditures"."per_pupil_expenditure" DESC;
-
-SELECT AVG("expenditures"."per_pupil_expenditure")
-FROM "expenditures";
