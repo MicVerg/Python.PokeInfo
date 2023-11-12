9f9068a9386e4c3a9a22b51d1f48d5492b670e42
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-11-12 13:58:51 +0000

    Sun, Nov 12, 2023, 2:58 PM +01:00

--- dese/12.sql
+++ dese/12.sql
@@ -1,9 +1,10 @@
-SELECT "districts"."name", "expenditures"."per_pupil_expenditure", "staff_evaluation"."exemplary"
+SELECT "districts"."name", "expenditures"."per_pupil_expenditure", "staff_evaluations"."exemplary"
 FROM "districts"
 JOIN "expenditures" ON "districts"."id" = "expenditures"."district_id"
-JOIN "staff_evaluation" ON "districts"."id" = "staff_evaluation"."district_id"
+JOIN "staff_evaluations" ON "districts"."id" = "staff_evaluations"."district_id"
 WHERE "expenditures"."per_pupil_expenditure" > (SELECT AVG("expenditures"."per_pupil_expenditure") FROM "expenditures")
-AND "
+AND "staff_evaluations"."exemplary" > (SELECT AVG("staff_evaluations"."exemplary") FROM "staff_evaluations")
+ORDER BY "staff_evaluations"."exemplary" DESC, "expenditures"."per_pupil_expenditure" DESC;
 
 SELECT AVG("expenditures"."per_pupil_expenditure")
 FROM "expenditures";
