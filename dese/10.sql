3c4b647e330deb14acfb5c5ba20bf5c3fe8cf155
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-11-12 13:38:46 +0000

    Sun, Nov 12, 2023, 2:38 PM +01:00

--- dese/10.sql
+++ dese/10.sql
@@ -1,7 +1,7 @@
 SELECT "districts"."name", "expenditures"."per_pupil_expenditure"
 FROM "expenditures"
-WHERE "districts"."type" = 'Public School District'
 JOIN "districts"
 ON "expenditures"."id" = "districts"."id"
+WHERE "districts"."type" = 'Public School District'
 ORDER BY "expenditures"."per_pupil_expenditure" DESC
 LIMIT 10;
