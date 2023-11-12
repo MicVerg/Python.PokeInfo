9d05897d36349d935852fd1a0ee1848ca6647b96
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-11-12 13:51:32 +0000

    Sun, Nov 12, 2023, 2:51 PM +01:00

--- dese/12.sql
+++ dese/12.sql
@@ -1,2 +1,5 @@
 SELECT "districts"."name", "expenditures"."per_pupil_expenditure", "staff_evaluation"."exemplary"
 FROM "districts"
+JOIN "expenditures" ON "districts"."id" = "expenditures"."district_id"
+JOIN "staff_evaluation" ON "districts"."id" = "staff_evaluation"."district_id"
+WHERE 
