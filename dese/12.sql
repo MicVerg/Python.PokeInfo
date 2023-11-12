02e74076dbe1158d4c9243b50b97322cb4c11c8e
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-11-12 13:55:03 +0000

    Sun, Nov 12, 2023, 2:55 PM +01:00

--- dese/12.sql
+++ dese/12.sql
@@ -2,4 +2,8 @@ SELECT "districts"."name", "expenditures"."per_pupil_expenditure", "staff_evalua
 FROM "districts"
 JOIN "expenditures" ON "districts"."id" = "expenditures"."district_id"
 JOIN "staff_evaluation" ON "districts"."id" = "staff_evaluation"."district_id"
-WHERE 
+WHERE "expenditures"."per_pupil_expenditure" > (SELECT AVG("expenditures"."per_pupil_expenditure") FROM "expenditures")
+AND
+
+SELECT AVG("expenditures"."per_pupil_expenditure")
+FROM "expenditures";
