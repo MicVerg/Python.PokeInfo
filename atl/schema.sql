260ccdac638216d4c6c0d7e26e2e528580b1dc87
DwoazePipo <130508259+DwoazePipo@users.noreply.github.com>
2023-11-22 08:58:21 +0000

    Wed, Nov 22, 2023, 9:58 AM +01:00

---
+++ atl/schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE "passengers"(
+    "id" INTEGER,
+    "first_name" TEXT,
+    "last_name" TEXT,
+    "age" NUMERIC,
+    PRIMARY KEY ("id")
+);
+
+CREATE TABLE "check-ins"(
+    "id" INTEGER
+)
